8f9197be4db1766299d43fb4e41bd115c8c5adf7
msafih <msafi+hollowverse@msafi.com>
2017-04-10 14:49:07 -0700

    Automatically deploy master (#38)

--- deploy.ts
+++ deploy.ts
@@ -1,11 +1,13 @@
 import * as chalk from 'chalk'
 import * as shelljs from 'shelljs'
 
+(shelljs as any).set('-e')
+
 print(chalk.green.bold('Building JavaScript and CSS files...'))
 shelljs.exec('npm run client/build')
 
 print(chalk.green.bold('Deploying to Firebase...'))
-shelljs.exec('"./node_modules/.bin/firebase" deploy')
+shelljs.exec('"./node_modules/.bin/firebase" deploy --token ${FIREBASE_TOKEN}')
 
 print(chalk.green.bold('Done'))
 
