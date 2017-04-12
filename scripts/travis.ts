c0e0937b00f0ad5d42dd7407d13202ae985ca39a
M.K. Safi <msafi+hollowverse@msafi.com>
2017-04-11 20:42:21 -0700

    Forgot to add `scripts/`

--- scripts/travis.ts
+++ scripts/travis.ts
@@ -3,4 +3,4 @@ import * as shelljs from 'shelljs'
 (shelljs as any).set('-e')
 
 shelljs.exec('npm run test')
-shelljs.exec('"./node_modules/.bin/ts-node" deploy.ts')
+shelljs.exec('"./node_modules/.bin/ts-node" scripts/deploy.ts')
