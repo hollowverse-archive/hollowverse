46a9bda0979c14bb961c4da172296d3eccc87f30
msafih <msafi+hollowverse@msafi.com>
2017-04-11 21:38:14 -0700

    Fix Travis CI deployment (take 2) (#41)
    * Fix Travis CI deployment (take 2)
    
    * Don't deploy PRs!
    
    * Use one script for Travis, and fix encrypted FIREBASE_TOKEN
    
    * Forgot to add `scripts/`
    
    * Maybe this will fix `firebase deploy`?
    
    * For some reason it needs a new FIREBASE_TOKEN
    
    * Try with `--add` flag?
    
    * One more time
    
    * Like this?
    
    * `npm i` in Firebase `functions/`
    
    * It finally worked, let's re-enable `master` and PR checks
    
    * Clean up

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,11 +3,18 @@ import * as shelljs from 'shelljs'
 
 (shelljs as any).set('-e')
 
+print(chalk.green.bold('Deployment started...'))
+
 print(chalk.green.bold('Building JavaScript and CSS files...'))
 shelljs.exec('npm run client/build')
 
+print(chalk.green.bold('`npm install` in Firebase `functions/`...'))
+shelljs.cd('functions/')
+shelljs.exec('npm install')
+shelljs.cd('..')
+
 print(chalk.green.bold('Deploying to Firebase...'))
-shelljs.exec('"./node_modules/.bin/firebase" deploy --token ${FIREBASE_TOKEN}')
+shelljs.exec(`"./node_modules/.bin/firebase" deploy --token "${shelljs.env['FIREBASE_TOKEN']}"`)
 
 print(chalk.green.bold('Done'))
 
---
+++ scripts/travis.ts
@@ -0,0 +1,12 @@
+import * as shelljs from 'shelljs'
+
+(shelljs as any).set('-e')
+
+shelljs.exec('npm run test')
+
+if (
+  shelljs.env['TRAVIS_BRANCH'] === 'master' &&
+  shelljs.env['TRAVIS_PULL_REQUEST'] === 'false'
+) {
+  shelljs.exec('"./node_modules/.bin/ts-node" scripts/deploy.ts')
+}
