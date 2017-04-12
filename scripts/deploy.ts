e006cadffacbb58d5a79991c7e52a1f9536b4722
M.K. Safi <msafi+hollowverse@msafi.com>
2017-04-11 20:48:13 -0700

    Maybe this will fix `firebase deploy`?

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -16,7 +16,7 @@ print(chalk.green.bold('Building JavaScript and CSS files...'))
 shelljs.exec('npm run client/build')
 
 print(chalk.green.bold('Deploying to Firebase...'))
-shelljs.exec('"./node_modules/.bin/firebase" deploy --token ${FIREBASE_TOKEN}')
+shelljs.exec(`"./node_modules/.bin/firebase" deploy --token ${shelljs.env['FIREBASE_TOKEN']}`)
 
 print(chalk.green.bold('Done'))
 
