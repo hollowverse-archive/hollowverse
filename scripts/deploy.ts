b7bb51559b33c928e151b18b2c154bd9774a2d72
M.K. Safi <msafi+hollowverse@msafi.com>
2017-04-11 21:33:08 -0700

    Clean up

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,13 +3,6 @@ import * as shelljs from 'shelljs'
 
 (shelljs as any).set('-e')
 
-if (
-  shelljs.env['TRAVIS_BRANCH'] !== 'master' ||
-  shelljs.env['TRAVIS_PULL_REQUEST'] !== 'false'
-) {
-  shelljs.exit(0)
-}
-
 print(chalk.green.bold('Deployment started...'))
 
 print(chalk.green.bold('Building JavaScript and CSS files...'))
--- scripts/travis.ts
+++ scripts/travis.ts
@@ -3,4 +3,10 @@ import * as shelljs from 'shelljs'
 (shelljs as any).set('-e')
 
 shelljs.exec('npm run test')
-shelljs.exec('"./node_modules/.bin/ts-node" scripts/deploy.ts')
+
+if (
+  shelljs.env['TRAVIS_BRANCH'] === 'master' &&
+  shelljs.env['TRAVIS_PULL_REQUEST'] === 'false'
+) {
+  shelljs.exec('"./node_modules/.bin/ts-node" scripts/deploy.ts')
+}
