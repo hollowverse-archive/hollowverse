818d69bb022b8bf0086eb7e8edcff9c30eb04e61
M.K. Safi <msafi+hollowverse@msafi.com>
2017-04-28 16:53:53 -0700

    Add `createSingleActionReducer` that accepts a function

--- client/src/redux/reducers.ts
+++ client/src/redux/reducers.ts
@@ -62,35 +62,18 @@ const initialState: State = {
   routing: undefined,
 }
 
-// For each Redux action, there would be at least one corresponding state key to be patched.
-//
-// For example, say you dispatch an action `setLoginStatus('connected')`, you
-// would have to have a reducer that will patch the state key `loginStatus` with the value `'connected'`.
-//
-// What this function does is: for any payload that simply needs to be inserted into the state without any logic
-// it will return a function that inserts that payload into the state and returns the new state.
-//
-// You can see how it's being used below with the `singleActionReducers` object.
-//
-// We can do away with this function, but then we'll have a bunch of boilerplate for each key we patch.
-function createSingleActionReducer<PayloadType>(stateKeyToPatch: string) {
-  return function simpleStatePatcher(state: AppState, action: Action<PayloadType>): AppState {
-    return ({...state, [stateKeyToPatch]: action.payload})
-  }
-}
-
-// We get the `ActionTypes` from `/redux/actions.ts` and for each one, we create an appropriate patcher.
+// We get the `ActionTypes` from `/redux/actions.ts` and for each one, we create an appropriate reducer.
 const singleActionReducers = {
-  [ActionTypes.setSearchInputValue]: createSingleActionReducer<string>('searchInputValue'),
-  [ActionTypes.setSearchResults]: createSingleActionReducer<IAlgoliaSearchResults | undefined>('searchResults'),
-  [ActionTypes.setIsSearchPending]: createSingleActionReducer<boolean>('isSearchPending'),
-  [ActionTypes.setLoginStatus]: createSingleActionReducer<facebookSdk.LoginStatus>('loginStatus'),
-  [ActionTypes.setIsLoginPending]: createSingleActionReducer<boolean>('isLoginPending'),
-  [ActionTypes.setIsLogoutPending]: createSingleActionReducer<boolean>('isLogoutPending'),
-  [ActionTypes.setError]: createSingleActionReducer<HvError>('error'),
-  [ActionTypes.setIsNavMenuOpen]: createSingleActionReducer<boolean>('isNavMenuOpen'),
-  [ActionTypes.setLastSearchTerm]: createSingleActionReducer<string>('lastSearchTerm'),
-  [ActionTypes.setCreateProfileUrlInputValue]: createSingleActionReducer<string>('createProfileUrlInputValue'),
+  [ActionTypes.setSearchInputValue]: createSingleActionSimpleReducer<string>('searchInputValue'),
+  [ActionTypes.setSearchResults]: createSingleActionSimpleReducer<IAlgoliaSearchResults | undefined>('searchResults'),
+  [ActionTypes.setIsSearchPending]: createSingleActionSimpleReducer<boolean>('isSearchPending'),
+  [ActionTypes.setLoginStatus]: createSingleActionSimpleReducer<facebookSdk.LoginStatus>('loginStatus'),
+  [ActionTypes.setIsLoginPending]: createSingleActionSimpleReducer<boolean>('isLoginPending'),
+  [ActionTypes.setIsLogoutPending]: createSingleActionSimpleReducer<boolean>('isLogoutPending'),
+  [ActionTypes.setError]: createSingleActionSimpleReducer<HvError>('error'),
+  [ActionTypes.setIsNavMenuOpen]: createSingleActionSimpleReducer<boolean>('isNavMenuOpen'),
+  [ActionTypes.setLastSearchTerm]: createSingleActionSimpleReducer<string>('lastSearchTerm'),
+  [ActionTypes.setCreateProfileUrlInputValue]: createSingleActionSimpleReducer<string>('createProfileUrlInputValue'),
 }
 
 // A Redux reducer is simply a function that accepts `state` and `action` and returns a new `state`.
@@ -115,5 +98,43 @@ export const reducer: Reducer<State> = (state: State = initialState, action: Act
   return {
     ...appReducer(state, action),
     routing: routerReducer(state.routing, action),
+  }
+}
+
+//
+// PRIVATE FUNCTIONS
+//
+//
+
+// For each Redux action, there would be at least one corresponding state key to be patched.
+//
+// For example, say you dispatch an action `setLoginStatus('connected')`, you
+// would have to have a reducer that will patch the state key `loginStatus` with the value `'connected'`.
+//
+// What this function does is: for any payload that simply needs to be inserted into the state without any logic
+// it will return a function that inserts that payload into the state and returns the new state.
+//
+// You can see how it's being used below with the `singleActionReducers` object.
+//
+// We can do away with this function, but then we'll have a bunch of boilerplate for each key we patch.
+function createSingleActionSimpleReducer<PayloadType>(stateKeyToPatch: keyof IAppState) {
+  return function singleActionSimpleReducer(state: AppState, action: Action<PayloadType>): AppState {
+    return ({...state, [stateKeyToPatch]: action.payload})
+  }
+}
+
+// If you need to reduce a state property by using logic, you can use `createSingleActionReducer` below.
+// It accepts one argument, a `patchingFunction`.
+//
+// For example:
+//
+// someActionType: createSingleActionReducer<boolean>(function(action, state) {
+//   return {setIsLoginPending: !state.isLoginPending}
+// })
+function createSingleActionReducer<PayloadType>(
+  patchingFunction: (action: Action<PayloadType>, state: AppState) => Partial<AppState>,
+) {
+  return function singleActionReducer(action: Action<PayloadType>, state: AppState) {
+    return {...state, ...patchingFunction(action, state)}
   }
 }
