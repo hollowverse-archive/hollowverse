9695cb5a3f3f77d5fa879c5e5fbada821b720aa5
M.K. Safi <msafi+hollowverse@msafi.com>
2017-04-27 21:09:48 -0700

    Move state objects back to `redux/reducers`

--- client/src/redux/actions.ts
+++ client/src/redux/actions.ts
@@ -12,34 +12,6 @@ import {HvError} from '../typeDefinitions'
 import {handleAction} from './utils'
 import {push} from 'react-router-redux'
 
-// This interface defines types of the entire state object of Hollowverse
-export interface IGeneralState {
-  searchInputValue: string,
-  searchResults: IAlgoliaSearchResults | undefined,
-  isSearchPending: boolean,
-  loginStatus: facebookSdk.LoginStatus,
-  isLoginPending: boolean,
-  isLogoutPending: boolean,
-  error: HvError,
-  isNavMenuOpen: boolean,
-  lastSearchTerm: string,
-  createProfileUrlInputValue: string
-}
-
-// Initialize the default state object
-export const initialGeneralState: IGeneralState = {
-  searchInputValue: '',
-  searchResults: undefined,
-  isSearchPending: false,
-  loginStatus: 'unknown',
-  isLoginPending: false,
-  isLogoutPending: false,
-  error: undefined,
-  isNavMenuOpen: false,
-  lastSearchTerm: '',
-  createProfileUrlInputValue: '',
-}
-
 // These are all the actions that can be triggered from within the Hollowverse application
 export const actions = {
   navigateToSearch: (payload: string) => {
@@ -58,11 +30,13 @@ export const actions = {
   setError: (payload: HvError) => ({type: 'setError', payload}),
   setIsNavMenuOpen: (payload: boolean) => ({type: 'setIsNavMenuOpen', payload}),
   setLastSearchTerm: (payload: string) => ({type: 'setLastSearchTerm', payload}),
+  setCreateProfileUrlInputValue: (payload: string) => ({type: 'setCreateProfileUrlInputValue', payload}),
+
+  // Redux Sagas actions
   requestSearchResults: (payload: string) => ({type: 'requestSearchResults', payload}),
   requestLogin: () => ({type: 'requestLogin'}),
   requestLogout: () => ({type: 'requestLogout'}),
   requestUpdateLoginStatus: () => ({type: 'requestUpdateLoginStatus'}),
-  setCreateProfileUrlInputValue: (payload: string) => ({type: 'setCreateProfileUrlInputValue', payload}),
 }
 
 export type ActionTypes = keyof typeof actions
--- client/src/redux/reducers.ts
+++ client/src/redux/reducers.ts
@@ -1,9 +1,38 @@
 import {Action} from './utils'
 import {Reducer} from 'redux'
 import {routerReducer, RouterState} from 'react-router-redux'
-import {IGeneralState, initialGeneralState, actionHandlers} from './actions'
-
+import {actionHandlers} from './actions'
 import LoginStatus = facebookSdk.LoginStatus
+import {IAlgoliaSearchResults} from '../vendor/algolia'
+import {HvError} from '../typeDefinitions'
+
+// This interface defines types of the entire state object of Hollowverse
+export interface IGeneralState {
+  searchInputValue: string,
+  searchResults: IAlgoliaSearchResults | undefined,
+  isSearchPending: boolean,
+  loginStatus: facebookSdk.LoginStatus,
+  isLoginPending: boolean,
+  isLogoutPending: boolean,
+  error: HvError,
+  isNavMenuOpen: boolean,
+  lastSearchTerm: string,
+  createProfileUrlInputValue: string
+}
+
+// Initialize the default state object
+export const initialGeneralState: IGeneralState = {
+  searchInputValue: '',
+  searchResults: undefined,
+  isSearchPending: false,
+  loginStatus: 'unknown',
+  isLoginPending: false,
+  isLogoutPending: false,
+  error: undefined,
+  isNavMenuOpen: false,
+  lastSearchTerm: '',
+  createProfileUrlInputValue: '',
+}
 
 interface IState extends IGeneralState {
   routing: RouterState | undefined
