092904f68824a697faf38ba4f7f7a8deaef52310
levsthings <lev.esenish@gmail.com>
2017-05-02 02:23:23 +0300

    fix commas

--- typings/circular-dependency-plugin.d.ts
+++ typings/circular-dependency-plugin.d.ts
@@ -2,14 +2,15 @@ declare module 'circular-dependency-plugin' {
   import { Compiler, Plugin } from 'webpack'
 
   interface CircularDependencyPluginOptions {
-    exclude?: RegExp
+    exclude?: RegExp,
     failOnError?: boolean
   }
 
   class CircularDependencyPlugin implements Plugin {
-    constructor(options?: CircularDependencyPluginOptions);
+    constructor(options?: CircularDependencyPluginOptions)
     apply(compiler: Compiler): void
   }
+
   namespace CircularDependencyPlugin { }
 
   export = CircularDependencyPlugin
