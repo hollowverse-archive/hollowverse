1b1bc45de8eed87d5f2f50abf672d25df1329481
Lev E <levsthings@users.noreply.github.com>
2017-05-07 20:58:18 +0300

    change how temporary styles from Bulma is included in the project (#72)

--- client/src/app/header.tsx
+++ client/src/app/header.tsx
@@ -31,41 +31,49 @@ class HeaderClass extends React.Component<ActionCreators & Props, undefined> {
   render() {
     const {props: p} = this
     const navClass = (p.isNavMenuOpen) ? 'is-active' : ''
-
-    return (
-        <nav className='nav has-shadow'>
-          <div className='container'>
-            <div className='nav-left'>
-              <div className='nav-item'>
-                <h1 className='title'><Link to='/'>Hollowverse</Link></h1>
-              </div>
-            </div>
-
-            <span
-              className={cn('nav-toggle ignore-react-onclickoutside', navClass)}
-              onClick={() => this.toggleIsNavMenuOpen()}
-            >
-              <span/>
-              <span/>
-              <span/>
-            </span>
-
-            {this.renderMenuItems(navClass, {isHiddenMobile: true})}
-
-            <FadeInDown>
-              {p.isNavMenuOpen && (
-                <OnClickOutside handleClickOutside={() => p.setIsNavMenuOpen(false)}>
-                  {this.renderMenuItems(navClass)}
-                </OnClickOutside>
-              )}
-            </FadeInDown>
+    /*
+      return (
+          <div className={css(styles.navBar)}>
+            <img className={css(styles.iconMenu)} src='assets/menu.svg'/>
+            <h1 className={css(styles.textLogo)}>Hollowverse</h1>
+            <img className={css(styles.iconSignIn)} src='assets/sign-in.svg'/>
           </div>
-        </nav>
+      )
+      */
+    return (
+      <nav className='nav has-shadow'>
+        <div className='container'>
+          <div className='nav-left'>
+            <div className='nav-item'>
+              <h1 className='title'><Link to='/'>Hollowverse</Link></h1>
+            </div>
+          </div>
+
+          <span
+            className={cn('nav-toggle ignore-react-onclickoutside', navClass)}
+            onClick={() => this.toggleIsNavMenuOpen()}
+          >
+            <span/>
+            <span/>
+            <span/>
+          </span>
+
+          {this.renderMenuItems(navClass, {isHiddenMobile: true})}
+
+          <FadeInDown>
+            {p.isNavMenuOpen && (
+              <OnClickOutside handleClickOutside={() => p.setIsNavMenuOpen(false)}>
+                {this.renderMenuItems(navClass)}
+              </OnClickOutside>
+            )}
+          </FadeInDown>
+        </div>
+      </nav>
     )
   }
 
   renderMenuItems(navClass: string, config: {isHiddenMobile?: boolean} = {}) {
-    const {props: p} = this
+    const { props: p } = this
 
     return (
       <div
@@ -75,11 +83,11 @@ class HeaderClass extends React.Component<ActionCreators & Props, undefined> {
         {(p.loginStatus === 'connected') && (
           <a className='nav-item' onClick={() => p.requestLogout()}>Logout</a>
         ) || (
-          <a className='nav-item' onClick={() => p.requestLogin()}>
-            Login with Facebook
+            <a className='nav-item' onClick={() => p.requestLogin()}>
+              Login with Facebook
             <Icon name='facebook-official' size={16} className='facebookIcon'/>
-          </a>
-        )}
+            </a>
+          )}
       </div>
     )
   }
