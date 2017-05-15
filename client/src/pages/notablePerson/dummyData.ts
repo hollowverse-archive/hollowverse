29f32f273a3da62298a4c49b4d7d117ab11b26f9
levsthings <lev.esenish@gmail.com>
2017-05-15 14:38:46 +0300

    extend dummy data for events

---
+++ client/src/pages/notablePerson/dummyData.ts
@@ -0,0 +1,25 @@
+export const data = {
+  notablePersonName: 'Michael Jackson',
+  notablePersonPictureUrl: 'http://lorempixel.com/150/150/',
+  notablePersonLabels: [
+    'Musician',
+    'Non-Political',
+    "Jehova's Witness",
+  ],
+  notablePersonEvents: [
+    {
+      eventId: 100,
+      eventQuote: 'My opinions are public schools are...',
+      eventSource: 'http://tmz.io/mjackson/bigevent',
+      comment: 'I love how Michael put this issue forward',
+      addedBy: 'Mr Cookie',
+    },
+    {
+      eventId: 101,
+      eventQuote: 'I voted for Octocat!',
+      eventSource: 'http://politico.io/mj/ballotnews',
+      comment: 'He admitted voting for Octocat',
+      addedBy: 'Awesome Gal',
+    },
+  ],
+}
--- client/src/pages/notablePerson/notablePerson.tsx
+++ client/src/pages/notablePerson/notablePerson.tsx
@@ -3,26 +3,25 @@ import * as React from 'react'
 import {RouteComponentProps} from 'react-router-dom'
 import {common} from '../../common.styles'
 import {styles} from './notablePerson.styles'
+import {data} from './dummyData'
 
 interface IProps {
   notablePersonName: string
   notablePersonPictureUrl: string
   notablePersonLabels: string[]
-}
-
-const dummyData: IProps = {
-  notablePersonName: 'Michael Jackson',
-  notablePersonPictureUrl: 'http://lorempixel.com/150/150/',
-  notablePersonLabels: [
-    'Musician',
-    'Non-Political',
-    "Jehova's Witness",
-  ],
+  notablePersonEvents: {
+    eventId: number,
+    eventQuote: string,
+    eventSource: string,
+    comment: string,
+    addedBy: string,
+  }[]
 }
 
 /* Todo:
   - [x] Custom Styles
   - [x] Custom Labels
+    [ ] Add Events
   - [ ] Second pass for design & code optimization
 */
 
@@ -33,10 +32,10 @@ class NotablePersonClass extends React.Component<ComponentProps, undefined> {
     return (
       <div className={css(common.page)}>
         <div className={css(styles.notablePersonTitleContainer)}>
-          <img className={css(styles.notablePersonPhoto)} src={dummyData.notablePersonPictureUrl}/>
+          <img className={css(styles.notablePersonPhoto)} src={data.notablePersonPictureUrl}/>
           <div className={css(styles.notablePersonText)}>
             <h1 className={css(styles.notablePersonTitle)}>Religion, politics, and ideas of...</h1>
-            <h2 className={css(common.titleTypography, styles.notablePersonName)}>{dummyData.notablePersonName}</h2>
+            <h2 className={css(common.titleTypography, styles.notablePersonName)}>{data.notablePersonName}</h2>
             {this.renderLabels()}
           </div>
         </div>
@@ -44,7 +43,7 @@ class NotablePersonClass extends React.Component<ComponentProps, undefined> {
     )
   }
   renderLabels() {
-    const { notablePersonLabels } = dummyData
+    const { notablePersonLabels } = data
     return (
       notablePersonLabels.map((label, i) =>
         <span className={css(styles.notablePersonLabel)} key={i}>
