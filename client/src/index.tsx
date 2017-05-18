0f28aee3bb1f3541a54aaa30afc3bc25491d2f43
Erika Ritter <erikajritter@gmail.com>
2017-05-18 04:47:55 -0400

    Uncomment, update import contactUsForm, snakecase route path

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -10,7 +10,7 @@ import {Homepage} from './pages/homepage/homepage'
 import Login from './pages/login/login'
 import {NotablePerson} from './pages/notablePerson/notablePerson'
 import {history, store} from './redux/store'
-// import ContactUs from './pages/contactUs/contactUs'
+import {ContactUsForm} from './pages/contactUsForm/contactUsForm'
 // import Search from "./page/search/search"
 
 ReactDOM.render(
@@ -23,7 +23,7 @@ ReactDOM.render(
         {/*/!*<Route path="/create-profile" component={requireUserLogin(CreateProfile)}/>*!/*/}
         {/*/!*<Route path="/search" component={Search}/>*!/*/}
         {/*/!*<Route path="/login" component={Login}/>*!/*/}
-        {/*/!*<Route path='/contactus' component={ContactUs}/>*!/*/}
+        {<Route path='/contactus' component={ContactUsForm}/>}
       </App>
     </ConnectedRouter>
   </Provider>,
