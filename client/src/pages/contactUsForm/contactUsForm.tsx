2e7e2beea925378ab408a9e0c854b83e68dddcab
Erika Ritter <erikajritter@gmail.com>
2017-05-24 19:19:08 -0400

    Create <Input /> component

--- client/src/pages/contactUsForm/contactUsForm.tsx
+++ client/src/pages/contactUsForm/contactUsForm.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import {connect} from 'react-redux'
 import {RouteComponentProps} from 'react-router-dom'
+import {Input} from '../../components/input'
 import {actions} from '../../redux/actions'
 import {State} from '../../redux/reducers'
 import {IContactFormData} from '../../../../typings/typeDefinitions'
@@ -33,54 +34,12 @@ type ComponentProps = ActionCreators & IProps & RouteComponentProps<any>
 
 class ContactUsFormClass extends React.Component<ComponentProps, undefined> {
   render() {
-    const {props: p} = this
-
     return (
       <div>
-        <h1>Contact Us</h1>
-        <form onSubmit={(event) => this.handleFormSubmit(event)} noValidate>
-          <label>
-            <input
-              placeholder='Your Email'
-              type='email'
-              value={p.emailInputValue}
-              onChange={({target: {value}}) => this.handleEmailInputChange(value)}
-            />
-          </label>
-          <label>
-            <textarea
-              placeholder='Your Message'
-              value={p.messageInputValue}
-              onChange={({target: {value}}) => this.handleMessageInputChange(value)}
-              >
-            </textarea>
-          </label>
-          <input
-            disabled={!isValidEmail(p.emailInputValue) || !hasSentence(p.messageInputValue)}
-            type='submit'
-            value='Submit'
-          />
-        </form>
+        <Input />
       </div>
     )
   }
-
-  handleEmailInputChange(emailText: string) {
-    const {props: p} = this
-    p.setEmailInputValue(emailText)
-  }
-
-  handleMessageInputChange(messageText: string) {
-    const {props: p} = this
-    p.setMessageInputValue(messageText)
-  }
-
-  handleFormSubmit(event: any) {
-    const {props: p} = this
-    event.preventDefault()
-    p.setSubmitFormValues({email: p.emailInputValue, message: p.messageInputValue})
-  }
-
 }
 
 export const ContactUsForm = connect<IProps, ActionCreators, RouteComponentProps<any>>(
