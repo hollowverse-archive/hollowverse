8b66551c8dda0e1dcfe4214b04f8c72c98ba160a
levsthings <lev.esenish@gmail.com>
2017-05-25 16:18:49 +0300

    refactor all facebook functions

--- client/src/vendor/facebook.ts
+++ client/src/vendor/facebook.ts
@@ -1,66 +1,34 @@
 import {errors} from '../constants/errors'
 
-/*
-export function getLoginStatus() {
-  return new Promise<facebookSdk.IAuthResponse>((resolve) => {
-    FB.getLoginStatus((response) => {
-      resolve(response)
-    })
-  })
-}
-*/
-
-// WIP: saga doesn't like the response.
 export async function getLoginStatus() {
   try {
-    return await FB.getLoginStatus((response) => {
-      return response
+    return await new Promise<facebookSdk.IAuthResponse>((resolve) => {
+      FB.getLoginStatus((response) => {
+        resolve(response)
+      })
     })
   } catch (err) {
     throw err
   }
 }
 
-/*
-export function login() {
-  return new Promise<facebookSdk.IAuthResponse>((resolve, reject) => {
-    FB.login((response) => {
-      if (response.authResponse) {
-        resolve(response)
-      } else {
-        reject(errors.facebookLoginError)
-      }
-    })
-  })
-}
-*/
-
-// WIP: untested
 export async function login() {
   try {
-    return await FB.login((response) => {
-      if (response.authResponse) {
-        return response
-      } else {
-        return errors.facebookLoginError
-      }
+    return await new Promise<facebookSdk.IAuthResponse>((resolve, reject) => {
+      FB.login((response) => {
+        if (response.authResponse) {
+          resolve(response)
+        } else {
+          reject(errors.facebookLoginError)
+        }
+      })
     })
   } catch (err) {
     throw err
   }
 }
-/*
-export function logout() {
-  return new Promise<facebookSdk.IAuthResponse>((resolve) => {
-    FB.logout((response) => {
-      resolve(response)
-    })
-  })
-}
-*/
 
-// WIP: untested
-export async function logout() {
+export async function logout(): Promise<facebookSdk.IAuthResponse | void> {
   try {
     return await FB.logout((response) => {
       return response
@@ -70,23 +38,7 @@ export async function logout() {
   }
 }
 
-/*
-export function initSdk() {
-  return new Promise<void>((resolve) => {
-    FB.init({
-      appId: '1151099935001443',
-      xfbml: true,
-      version: 'v2.8',
-      cookie: true,
-    })
-
-    resolve()
-  })
-}
-*/
-
-// Status: OK
-export async function initSdk() {
+export async function initSdk(): Promise<void> {
   try {
     return await FB.init({
       appId: '1151099935001443',
