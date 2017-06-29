8c5608c30a46713b8039eaabcefcb63f71bb18bf
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-06-29 22:45:48 +0300

    Rewrite webpack config
    * Add env.js file, which is a set of helper funtions to conditionally toggle webpack plugins/options based on the current environment (development/production/tests/CI...).
    * Rewrite Webpack config in JS instead of TS. While TS is perfect for our app code, it really gets in the way for configuration files. Most of the Webpack plugins we are using do not have type definitions, even the built-in plugins are missing some options. While we can write definition files for 3rd-party plugins, it looks like we cannot augment namespace imports with TypeScript. Moving to JS will also make Webpack config easier to consume by any tools we might add in the future (like React Storybook for example).
    * Add eslint to lint webpack.config.js and other JS files
    * Add support for global CSS
    * Add support for local CSS using CSS Modules (local CSS files should end with .module.scss or .module.css)
    * Support HMR in development to detect code changes and reload them faster.
    * Support SVG sprites for icons (although we are not currently using any SVG icons)
    * Use Preact instead of React by default
    * Split app into multiple chunks:
      * app.js contains our own components and routes.
      * react.js contains preact, preact-compat, react-router and all other React libraries.
      * vendor.js contains other non-React-specific code, like Firebase and Algolia integrations.
    * Dynamically generate index.html with all chunks configured correctly.
    * Set up long-term caching correctly with named modules and chunks hashed based on their content.
    * Minify JS with Babili by default
    * Preload files using <link rel=preload>
    * Enfore size limits on production assets
    
    * Add the following build flags:
      * NO_TYPE_CHECK disables type checking for the current build
      * NO_LINT disables linting of TS/JS/CSS files for the current build
      * NO_PERF_CHECKS disables enforced performance limits for the current build
      * ES5 transpiles TS/JS to ES5 instead of using babel-preset-env and uses UglifyJS for minification instead of Babili
      * REACT builds using React instead of Preact
    * Change client/dev, client/build to use the new Webpack configurtion
    * Add client/stats command to generate a stats.json file which contains break down of each chunk contents and corresponding size. This file can later be viewed in Webpack Visualized (https://chrisbateman.github.io/webpack-visualizer/) and can also be used in CI to monitor performance regressions
    * Add stylelint to lint CSS/Sass files

---
+++ env.js
@@ -0,0 +1,42 @@
+const compact = require('lodash/compact');
+
+exports.isTest = process.env.NODE_ENV === 'test';
+exports.isProduction = process.env.NODE_ENV === 'production';
+exports.isCI = Boolean(process.env.CI);
+exports.isDevelopment =
+  exports.isTest || process.env.NODE_ENV === 'development';
+exports.isDev = exports.isDevelopment;
+exports.isProd = exports.isProduction;
+exports.isHot = Boolean(process.env.HOT);
+exports.isBrowser = !process && typeof window !== 'undefined';
+exports.isPreact = !process.env.REACT;
+exports.isInferno = !exports.isPreact && Boolean(process.env.INFERNO);
+exports.isReact = !exports.isPreact && !exports.isInferno;
+exports.isES5 = Boolean(process.env.ES5);
+exports.isESNext = !exports.isES5;
+exports.isPerf = !process.env.NO_PERF_CHECKS;
+exports.shouldLint = !process.env.NO_LINT;
+exports.shouldTypeCheck = !process.env.NO_TYPE_CHECK;
+
+const createConditionalWithFallback = (
+  condition,
+  defaultFallback = undefined,
+) => (p, fallback = defaultFallback) => {
+  if (Array.isArray(p)) {
+    return condition ? compact(p) : fallback || [];
+  }
+  return condition ? p : fallback;
+};
+
+exports.ifProd = createConditionalWithFallback(exports.isProd);
+exports.ifHot = createConditionalWithFallback(exports.isHot);
+exports.ifTest = createConditionalWithFallback(exports.isTest);
+exports.ifDev = createConditionalWithFallback(exports.isDev);
+exports.ifReact = createConditionalWithFallback(exports.isReact);
+exports.ifPreact = createConditionalWithFallback(exports.isPreact);
+exports.ifInferno = createConditionalWithFallback(exports.isInferno);
+exports.ifES5 = createConditionalWithFallback(exports.isES5);
+exports.ifLint = createConditionalWithFallback(exports.shouldLint);
+exports.ifESNext = createConditionalWithFallback(exports.isESNext);
+exports.ifCI = createConditionalWithFallback(exports.isCI);
+exports.ifPerf = createConditionalWithFallback(exports.isPerf);
