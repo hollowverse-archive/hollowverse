67f905b1d25acf78a535f95b39a7ed08de5c5886
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-07-01 19:07:11 +0300

    Fix imprt path to reducer

--- server/src/redux.store.ts
+++ server/src/redux.store.ts
@@ -1,4 +1,4 @@
 import { createStore } from 'redux';
-import { reducer } from '../../client/src/redux/reducers';
+import { reducer } from '../../client/src/store/reducers';
 
 export const store = createStore(reducer);
