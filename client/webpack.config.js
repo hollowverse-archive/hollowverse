c9065526c8afdf53948bd084a0df20c1b688008d
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-07-03 00:48:39 +0300

    Try to fix CI
    * scripts are written in TS and use the shared tsconfig.json in project root. So I had to revert back to using ES2015 and CommonJS in tsconfig so the scripts can run in node.js
    * The previous configuration is now in webpack.config.js as it only affects the client.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -372,6 +372,8 @@ const config = {
               silent: true,
               transpileOnly: !env.shouldTypeCheck,
               compilerOptions: {
+                module: 'esnext',
+                target: 'esnext',
                 jsx: 'preserve',
                 noEmitOnError: env.shouldTypeCheck,
               },
