ef1d8e28316ca735ec56366c60852796a5f5b020
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-07-08 07:23:03 +0300

    Resolve PUBLIC_PATH relative to process cwd in server script

--- server/src/index.tsx
+++ server/src/index.tsx
@@ -2,14 +2,16 @@ import * as Express from 'express';
 import * as path from 'path';
 
 const app = Express();
+
 const PORT = process.env.PORT || 3000;
-const PUBLIC_PATH = process.env.PUBLIC_PATH || '../../public';
+const PUBLIC_PATH = process.env.PUBLIC_PATH
+  ? path.resolve(process.cwd(), process.env.PUBLIC_PATH)
+  : path.resolve(__dirname, '../../public');
 
-// This is fired every time the server side receives a request
-app.use(Express.static(path.resolve(PUBLIC_PATH)));
-
-app.get('*', (_, res) => {
+app.get('/', (_, res) => {
   return res.sendFile(path.resolve(PUBLIC_PATH, 'index.html'));
 });
+
+app.use(Express.static(path.resolve(PUBLIC_PATH)));
 
 app.listen(PORT);
