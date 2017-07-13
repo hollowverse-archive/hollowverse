55f0e2f0a7a17a45c55b3acfa06ae1c000c7ee3b
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-07-13 08:08:25 +0300

    Improve redirection proxy logic
    * As the proxy is placed in front of the old version, we need to allow requests to static assets to be directed to the new app.
    * The new proxy will check if the request is for a static file, and redirect accordingly.
    * As ":/path" matches routes on both new and old servers, the new proxy also has to know  the new app paths to avoid redirection loops.
    * A new npm script, "proxy" is introduced for testing the proxy in development, and is configured to restart when code changes.
    * The new app server has to be launched with "server/express" and the proxy should be started with the same port passed via PROXY_PORT env variable.

--- redirectionProxy/redirectionProxy.ts
+++ redirectionProxy/redirectionProxy.ts
@@ -1,25 +1,57 @@
 import * as express from 'express';
 import * as httpProxy from 'http-proxy';
-import { URL } from 'url';
+import * as fs from 'fs';
+import * as path from 'path';
 
 const server = express();
 
-// tslint:disable-next-line no-http-string
-const OLD_SERVER_ADDRESS = 'http://localhost:3000';
+// tslint:disable no-http-string no-suspicious-comment
+// @TODO: replace 'http://hollowverse.com' with old server address
+const OLD_SERVER_ADDRESS = process.env.OLD_SERVER || 'http://hollowverse.com';
+const NEW_SERVER_ADDRESS = process.env.NEW_SERVER || 'http://localhost:3000';
+// tslint:enable no-http-string no-suspicious-comment
+const PUBLIC_PATH = path.resolve(
+  process.cwd(),
+  process.env.PUBLIC_PATH || 'public',
+);
+const PROXY_PORT = process.env.PROXY_PORT || 8080;
 
-const proxyServer = httpProxy.createProxyServer({ target: OLD_SERVER_ADDRESS });
+const proxyServer = httpProxy.createProxyServer();
 
-const redirectionMap = new Map<string, string>([['tom-hanks', 'Tom_Hanks']]);
+const redirectionMap = new Map([['tom-hanks', 'Tom_Hanks']]);
 
-server.get('/:oldUrl', (req, res) => {
-  const oldSlug = req.params.oldUrl;
-  const newSlug = redirectionMap.get(oldSlug);
-  if (newSlug !== undefined) {
-    const url = new URL(newSlug, 'https://hollowverse.com');
-    res.redirect(url.toString());
+const newPaths = new Set(redirectionMap.values());
+const staticFiles = new Set(fs.readdirSync(PUBLIC_PATH));
+
+/*
+ * As the proxy is placed in front of the old version, we need to allow
+ * requests to static assets to be directed to the new app.
+ * The new proxy will check if the request is for a static file, and redirect accordingly.
+ * As ":/path" matches routes on both new and old servers, the new proxy also has
+ * to know  the new app paths to avoid redirection loops.
+ */
+server.get('/:path', (req, res, next) => {
+  // '/:path' matches: /Tom_Hanks, /tom-hanks, /app.js, /michael-jackson, ashton-kutcher...
+  const path: string = req.params.path;
+  const redirectionPath = redirectionMap.get(path);
+  if (redirectionPath !== undefined) {
+    // /tom-hanks => redirect to Tom_Hanks
+    res.redirect(redirectionPath);
+  } else if (newPaths.has(path) || staticFiles.has(path)) {
+    // /Tom_Hanks, /app.js, /vendor.js => new hollowverse
+    proxyServer.web(req, res, {
+      target: NEW_SERVER_ADDRESS,
+      changeOrigin: true,
+    });
   } else {
-    proxyServer.web(req, res);
+    // /michael-jackson, ashton-kutcher, / => old hollowverse
+    next();
   }
 });
 
-server.listen(80);
+// Fallback to old hollowverse
+server.use((req, res) =>
+  proxyServer.web(req, res, { target: OLD_SERVER_ADDRESS }),
+);
+
+server.listen(PROXY_PORT);
