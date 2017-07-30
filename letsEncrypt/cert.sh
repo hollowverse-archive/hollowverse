e08b8352357851fc7d5bd789ecfab3d4a6ff2149
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-07-30 22:53:26 +0300

    Get GAE certificate ID dynamically and act accordingly

--- letsEncrypt/cert.sh
+++ letsEncrypt/cert.sh
@@ -21,9 +21,15 @@ openssl rsa -in privkey.pem -out rsa.pem
 
 gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file /gae-client-secret.json
 
-# Upload fullchain.pem and rsa.pem to GAE
-# Disabled as the certificate is already uploaded in GAE, we just need to update it regularly
-# gcloud beta app ssl-certificates create --display-name $CERT_NAME --certificate ./fullchain.pem --private-key ./rsa.pem
+# Find the certificate ID that matches the certificate display name
+CERT_ID=$(gcloud beta app ssl-certificates list --filter $CERT_NAME --limit 1 | tail -n 1 | cut -f 1 -d ' ' | tr -d '[:space:]')
 
-# Instead, update the current one
-gcloud beta app ssl-certificates update $CERT_ID --certificate ./fullchain.pem --private-key ./rsa.pem
+# Upload fullchain.pem and rsa.pem to GAE
+if [[ -z $CERT_ID ]]; then
+  # Create a new certificate if no matching certificate ID is found
+  gcloud beta app ssl-certificates create --display-name $CERT_NAME --certificate ./fullchain.pem --private-key ./rsa.pem;
+else
+  # Otherwise, update the existing one
+  gcloud beta app ssl-certificates update $CERT_ID --certificate ./fullchain.pem --private-key ./rsa.pem
+fi
+
--- letsEncrypt/env.sh
+++ letsEncrypt/env.sh
@@ -2,4 +2,3 @@ export DOMAIN='hollowverse.com';
 export EMAIL='msafi@msafi.com';
 export SERVICE_ACCOUNT='travis@hollowverse-c9cad.iam.gserviceaccount.com';
 export CERT_NAME="lets-encrypt-$DOMAIN";
-export CERT_ID='553893';
