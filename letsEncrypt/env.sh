aafb92e08870226f7ccccade86c3dd3ba0ff425b
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-08-01 18:40:57 +0300

    Remove shell files and update scheduled commands to use JS version

--- letsEncrypt/env.sh
+++
@@ -1,4 +0,0 @@
-export DOMAIN='hollowverse.com';
-export EMAIL='msafi@msafi.com';
-export SERVICE_ACCOUNT='travis@hollowverse-c9cad.iam.gserviceaccount.com';
-export CERT_NAME="lets-encrypt-$DOMAIN";
