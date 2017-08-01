5c236d64a30c8f13590b5d53ef86b7dc0c1b08a5
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-08-01 20:03:47 +0300

    Fix CERT_NAME value

--- letsEncrypt/config.js
+++ letsEncrypt/config.js
@@ -1,6 +1,6 @@
 const DOMAIN = 'hollowverse.com';
 const EMAIL = 'msafi@msafi.com';
 const SERVICE_ACCOUNT = 'travis@hollowverse-c9cad.iam.gserviceaccount.com';
-const CERT_NAME = 'lets-encrypt-$DOMAIN';
+const CERT_NAME = `lets-encrypt-${DOMAIN}`;
 
 module.exports = { DOMAIN, EMAIL, SERVICE_ACCOUNT, CERT_NAME };
