bc5df29872106525a999ed5d32d46d6c2d529d6e
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-08-02 00:32:28 +0300

    Implement automated SSL certificate generation and renewal with Let's Encrypt (#135)
    * Add certbot to generate an SSL certificate
    
    * Move certificate generation logic to a dedicated App Engine service
    
    * Rename lets-encrypt to letsEncrypt
    
    * Ignore letsEncrypt in the default service
    
    * Add cron and crontab files to Let's Encrypt service
    
    * Use default directories for certbot
    
    * This is to allow the `certbot renew` command to find the certificates that need to be renewed.
    
    * Schedule renew twice a day
    
    * This will not necessarily update the certificate. certbot is smart enough to look at the expiration date of the certificate.
    
    * Schedule first-time certificate generation
    
    * Schedule at 30 minutes after deployment to ensure the service is up before attempting to verify domain ownership
    
    * Convert variables in cert.sh to env variables
    
    * Use Google Cloud SDK to upload certificates
    
    * Add update script
    
    * Fix lets-encrypt service build by moving the client secret to build context
    
    * Explain why the client secret file is added explicitly
    
    * Move environment variable to shell file
    
    * Looks like env variables are not supported in GAE for custom runtimes
    
    * Use bash to execute script as `source` is bash-specific
    
    * Use variables for gcloud command
    
    * Launch at daemon in lets-encrypt
    
    * Fix path to certificates
    
    * Fix path to service account key
    
    * Update client secret with exrta permissions
    
    In attempt to fix an access error when uploading the SSL certificate
    
    * Fix update commands. Do not create or update domain mapping
    
    This will fail as it's already done in the web console
    
    * Disable ssl create command as the certificate is already uploaded
    
    * Remove update script as it is now equivalent to the create script
    
    * Exit cert.sh early on first command failure
    
    * Get GAE certificate ID dynamically and act accordingly
    
    * Update the client secret again to fix the account name used in travis
    
    * Rewrite certificate script in JS
    
    * Handle failure in ssl-certificate list command
    
    * Remove shell files and update scheduled commands to use JS version
    
    * Run npm install in lets-encrypt service
    
    * Install dependencies for cert.js
    
    * Fix CERT_NAME value
    
    * Use absolute path for at command

---
+++ letsEncrypt/cert.js
@@ -0,0 +1,57 @@
+#! /bin/node
+const shelljs = require('shelljs');
+
+const { EMAIL, DOMAIN, CERT_NAME, SERVICE_ACCOUNT } = require('./config');
+
+shelljs.exec(`
+  certbot certonly \
+    --agree-tos \
+    --email ${EMAIL} \
+    --noninteractive \
+    --webroot \
+    --webroot-path ./public \
+    -d ${DOMAIN} \
+`);
+
+// This directory contains the certificate files, including the private key
+shelljs.cd(`/etc/letsencrypt/live/${DOMAIN}/`);
+
+// Convert private key to GAE-compatible format
+shelljs.exec('openssl rsa -in privkey.pem -out rsa.pem');
+
+// Authenticate to Google Cloud Platform
+shelljs.exec(
+  `gcloud auth activate-service-account ${SERVICE_ACCOUNT} --key-file /gae-client-secret.json`,
+);
+
+// Find the certificate ID that matches the certificate display name
+const result = shelljs.exec(
+  `gcloud beta app ssl-certificates list --filter ${CERT_NAME} --limit 1`,
+);
+
+if (result.code === 0) {
+  const CERT_ID = result.stdout
+    .split('\n')[1] // Take the second line of output (the first one is column headings)
+    .match(/^([0-9]+)\s/i)[1] // Get the certificate ID
+    .trim();
+
+  // Upload fullchain.pem and rsa.pem to GAE
+  if (CERT_ID.length === 0) {
+    // Create a new certificate if no matching certificate ID is found
+    shelljs.exec(`
+      gcloud beta app ssl-certificates create \
+      --display-name ${CERT_NAME} \
+      --certificate ./fullchain.pem \
+      --private-key ./rsa.pem 
+    `);
+  } else {
+    // Otherwise, update the existing one
+    shelljs.exec(`
+      gcloud beta app ssl-certificates update ${CERT_ID} \
+      --certificate ./fullchain.pem \
+      --private-key ./rsa.pem
+    `);
+  }
+} else {
+  console.error(`Failed to get the certificate list: ${result.stderr}`);
+}
