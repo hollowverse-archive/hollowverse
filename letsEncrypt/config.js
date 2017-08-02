4fa237206408fede6695c6b0016b92a5fc7f5a21
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-08-02 17:40:02 +0300

    Use default service account for deployment

--- letsEncrypt/config.js
+++ letsEncrypt/config.js
@@ -1,6 +1,6 @@
 const DOMAIN = 'hollowverse.com';
 const EMAIL = 'msafi@msafi.com';
-const SERVICE_ACCOUNT = 'travis@hollowverse-c9cad.iam.gserviceaccount.com';
+const SERVICE_ACCOUNT = 'hollowverse-c9cad@appspot.gserviceaccount.com';
 const CERT_NAME = `lets-encrypt-${DOMAIN}`;
 
 module.exports = { DOMAIN, EMAIL, SERVICE_ACCOUNT, CERT_NAME };
