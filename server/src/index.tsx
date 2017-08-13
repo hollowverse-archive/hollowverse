55c6a95613146f3e54e2a29a89e34c0904f46d32
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-08-13 19:41:24 +0300

    Move static server logic to a separate folder

--- server/src/index.tsx
+++ server/src/index.tsx
@@ -1,18 +1,11 @@
 import * as express from 'express';
-import * as path from 'path';
+
+import { staticServer } from './staticServer/staticServer';
 
 const app = express();
 
 const PORT = process.env.STATIC_SEVER_PORT || 3000;
-const PUBLIC_PATH = path.resolve(
-  process.cwd(),
-  process.env.PUBLIC_PATH || './public',
-);
 
-const indexFile = path.resolve(PUBLIC_PATH, 'index.html');
-
-app.use(express.static(PUBLIC_PATH));
-
-app.use((_, res) => res.sendFile(indexFile));
+app.use(staticServer);
 
 app.listen(PORT);
---
+++ server/src/staticServer/staticServer.ts
@@ -0,0 +1,17 @@
+import * as express from 'express';
+import * as path from 'path';
+
+const staticServer = express();
+
+const PUBLIC_PATH = path.resolve(
+  process.cwd(),
+  process.env.PUBLIC_PATH || './public',
+);
+
+const indexFile = path.resolve(PUBLIC_PATH, 'index.html');
+
+staticServer.use(express.static(PUBLIC_PATH));
+
+staticServer.use((_, res) => res.sendFile(indexFile));
+
+export { staticServer };
