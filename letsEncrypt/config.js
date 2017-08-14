fe89f608a2daa49d3bfd3c4fdedeb9775379edaa
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-08-14 21:50:26 +0300

    Update paths to service account key in letsEncrypt
    Also remove service acccount ID since it is already included in the key file so it is not required for `gcloud auth`.

--- letsEncrypt/config.js
+++ letsEncrypt/config.js
@@ -1,6 +1,5 @@
 const DOMAIN = 'hollowverse.com';
 const EMAIL = 'msafi@msafi.com';
-const SERVICE_ACCOUNT = 'travis@hollowverse-c9cad.iam.gserviceaccount.com';
 const CERT_NAME = `lets-encrypt-${DOMAIN}`;
 
-module.exports = { DOMAIN, EMAIL, SERVICE_ACCOUNT, CERT_NAME };
+module.exports = { DOMAIN, EMAIL, CERT_NAME };
