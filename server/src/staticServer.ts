c1224db5b6340e9232f6f46cbe0a309301aa7cd7
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-08-29 21:49:59 +0300

    Replace newServer with staticServer

--- server/src/staticServer.ts
+++ server/src/staticServer.ts
@@ -3,6 +3,8 @@ import * as path from 'path';
 
 const staticServer = express();
 
+const PORT = process.env.STATIC_SERVER_PORT || 3000;
+
 const PUBLIC_PATH = path.resolve(
   process.cwd(),
   process.env.PUBLIC_PATH || './client/dist',
@@ -14,4 +16,4 @@ staticServer.use(express.static(PUBLIC_PATH));
 
 staticServer.use((_, res) => res.sendFile(indexFile));
 
-export { staticServer };
+staticServer.listen(PORT);
