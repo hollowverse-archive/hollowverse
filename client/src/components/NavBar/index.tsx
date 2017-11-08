be13cde1ac4b3554ecd81402078ee85b459b9d44
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-11-08 18:28:19 +0200

    Make navbar title a link to home page

--- client/src/components/NavBar/index.tsx
+++ client/src/components/NavBar/index.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import './styles.scss';
 
 const NavBar = (props: { title: string }) => (
-  <div className="navbar">{props.title}</div>
+  <div className="navbar">
+    <a href="/">{props.title}</a>
+  </div>
 );
 
 export default NavBar;
