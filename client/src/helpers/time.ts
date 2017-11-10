bb4d64cb1f28c1b72278ececbb1c7980565783b6
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-11-10 21:45:39 +0200

    Fix another TSLint issue

--- client/src/helpers/time.ts
+++ client/src/helpers/time.ts
@@ -1,2 +1,7 @@
-export const delay = async (n = 3000, resolveValue?: any) =>
-  new Promise(resolve => setTimeout(() => resolve(resolveValue), n));
+export const delay = async (n = 3000, resolveValue?: any) => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(resolveValue);
+    }, n);
+  });
+};
