c90af8be247b60b8e0242c86e0e60f2d8c199383
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-11-11 21:16:33 +0200

    Replace fallback hostname

--- server/src/redirectToHttps.ts
+++ server/src/redirectToHttps.ts
@@ -5,7 +5,7 @@ const redirectToHttps = express();
 
 redirectToHttps.use((req, res, next) => {
   const protocol = req.header('X-FORWARDED-PROTO');
-  const host = req.header('Host') || 'api.hollowverse.com';
+  const host = req.header('Host') || 'hollowverse.com';
   if (protocol === 'http') {
     const newURL = new URL(req.url, `https://${host}`);
     res.redirect(newURL.toString());
