81855f521a2d306a21f3bc384dcc47a9d83c40c3
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-11-12 21:01:05 +0200

    Write type definitions for react-resolver

--- client/typings/react-resolver.d.ts
+++ client/typings/react-resolver.d.ts
@@ -1,1 +1,46 @@
-declare module 'react-resolver';
+declare module 'react-resolver' {
+  import { ComponentClass, StatelessComponent, Factory } from 'react';
+
+  interface AnyComponent<P> extends ComponentClass<P> {}
+
+  type AnyComponent<P> = ComponentClass<P> | StatelessComponent<P>;
+
+  export class Resolver {
+    static async resolve<P, D>(
+      factory: Factory<P>,
+    ): Promise<{
+      data: D;
+      Resolved: StatelessComponent<P>;
+    }>;
+
+    static render<P>(factory: Factory<P>, root: Node | null): void;
+  }
+
+  type ResolveFn<Props, V> = (props: Props) => Promise<V>;
+
+  export function resolve<
+    OwnProps,
+    K extends string,
+    V,
+    MoreProps = { [x: string]: any }
+  >(
+    prop: K,
+    resolveFn: ResolveFn<OwnProps & MoreProps, V>,
+  ): (
+    component: AnyComponent<OwnProps & { [K]?: V }>,
+  ) => StatelessComponent<OwnProps & MoreProps>;
+  export function resolve<
+    OwnProps,
+    ResolvableProps = { [x: string]: any },
+    MoreProps = { [x: string]: any }
+  >(
+    resolversMap: {
+      [K in keyof ResolvableProps]: ResolveFn<
+        OwnProps & MoreProps,
+        ResolvableProps[K]
+      >
+    },
+  ): (
+    component: AnyComponent<OwnProps & { [K]?: V }>,
+  ) => StatelessComponent<OwnProps & MoreProps>;
+}
