92cdfadd667f4ac540df325d7032a6864094397d
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-11-17 15:55:08 +0200

    Revert moving security middleware to bottom of code

--- src/mainServer.ts
+++ src/mainServer.ts
@@ -19,6 +19,35 @@ const server = express();
 
 server.use(redirectToHttps);
 
+server.use(
+  helmet({
+    hsts: {
+      // Enable HTTP Strict Transport Security
+      // This tells the browser to rewrite all subsequent http:// URLs to
+      // https:// so that we can skip the redirection request overhead.
+      maxAge: moment.duration(60, 'days').asSeconds(),
+      includeSubdomains: true,
+      preload: true,
+    },
+    hidePoweredBy: true,
+    noSniff: true,
+    ieNoOpen: true,
+    xssFilter: true,
+    frameguard: true,
+  }),
+);
+
+server.use(
+  helmet.referrerPolicy({
+    // Tells browsers that support the `Referrer-Policy` header to only send
+    // the `Referer` header when navigating to a secure origin.
+    // If the destination origin is different from the website's origin, the full URL
+    // is stripped so that it only contains the domain name.
+    // See https://www.w3.org/TR/referrer-policy/#referrer-policy-strict-origin-when-cross-origin
+    policy: 'strict-origin-when-cross-origin',
+  }),
+);
+
 // Add version details to custom header
 server.use((_, res, next) => {
   res.setHeader(
@@ -83,34 +112,5 @@ server.use((req, res) => {
     changeOrigin: true,
   });
 });
-
-server.use(
-  helmet({
-    hsts: {
-      // Enable HTTP Strict Transport Security
-      // This tells the browser to rewrite all subsequent http:// URLs to
-      // https:// so that we can skip the redirection request overhead.
-      maxAge: moment.duration(60, 'days').asSeconds(),
-      includeSubdomains: true,
-      preload: true,
-    },
-    hidePoweredBy: true,
-    noSniff: true,
-    ieNoOpen: true,
-    xssFilter: true,
-    frameguard: true,
-  }),
-);
-
-server.use(
-  helmet.referrerPolicy({
-    // Tells browsers that support the `Referrer-Policy` header to only send
-    // the `Referer` header when navigating to a secure origin.
-    // If the destination origin is different from the website's origin, the full URL
-    // is stripped so that it only contains the domain name.
-    // See https://www.w3.org/TR/referrer-policy/#referrer-policy-strict-origin-when-cross-origin
-    policy: 'strict-origin-when-cross-origin',
-  }),
-);
 
 server.listen(PORT);
