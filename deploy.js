28e13af513f70cb6a5bce09f08c4110877af2632
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-11-17 22:14:11 +0200

    Revert accidental changes to deploy.js

--- deploy.js
+++ deploy.js
@@ -1,28 +1,28 @@
 #! /usr/bin/env node
 
 const shelljs = require('shelljs');
-// const decryptSecrets = require('@hollowverse/common/helpers/decryptSecrets');
-// const executeCommand = require('@hollowverse/common/helpers/executeCommand');
+const decryptSecrets = require('@hollowverse/common/helpers/decryptSecrets');
+const executeCommand = require('@hollowverse/common/helpers/executeCommand');
 const executeCommands = require('@hollowverse/common/helpers/executeCommands');
 const writeJsonFile = require('@hollowverse/common/helpers/writeJsonFile');
 const createZipFile = require('@hollowverse/common/helpers/createZipFile');
 
 const {
-  // ENC_PASS_SUMO,
-  // IS_PULL_REQUEST,
+  ENC_PASS_SUMO,
+  IS_PULL_REQUEST,
   PROJECT,
   BRANCH,
   COMMIT_ID,
 } = shelljs.env;
 
-// const isPullRequest = IS_PULL_REQUEST !== 'false';
+const isPullRequest = IS_PULL_REQUEST !== 'false';
 
-// const secrets = [
-//   {
-//     password: ENC_PASS_SUMO,
-//     decryptedFilename: 'sumo.json',
-//   },
-// ];
+const secrets = [
+  {
+    password: ENC_PASS_SUMO,
+    decryptedFilename: 'sumo.json',
+  },
+];
 
 const ebEnvironmentName = `${PROJECT}-${BRANCH}`;
 
@@ -34,7 +34,7 @@ async function main() {
         BRANCH,
         COMMIT_ID,
       }),
-    // () => decryptSecrets(secrets, './secrets'),
+    () => decryptSecrets(secrets, './secrets'),
     () =>
       createZipFile(
         'build.zip',
@@ -50,30 +50,30 @@ async function main() {
         ['secrets/**/*.enc', 'secrets/**/*.d.ts'],
       ),
     // Use the Elastic Beanstalk environment of this branch (create it if necessary)
-    // () => {
-    //   const environments = shelljs
-    //     .exec('eb list')
-    //     .stdout.trim()
-    //     .split('\n');
-    //   if (environments.indexOf(ebEnvironmentName) === -1) {
-    //     return executeCommand(`eb create ${ebEnvironmentName}`);
-    //   }
-    //   return undefined;
-    // },
+    () => {
+      const environments = shelljs
+        .exec('eb list')
+        .stdout.trim()
+        .split('\n');
+      if (environments.indexOf(ebEnvironmentName) === -1) {
+        return executeCommand(`eb create ${ebEnvironmentName}`);
+      }
+      return undefined;
+    },
     `eb use ${ebEnvironmentName}`,
     'eb deploy --staged --debug',
   ];
 
-  const isDeployment = true;
-  // if (isPullRequest === true) {
-  //   console.info('Skipping deployment commands in PRs');
-  // } else if (secrets.some(secret => secret.password === undefined)) {
-  //   console.info(
-  //     'Skipping deployment commands because some secrets are not provided',
-  //   );
-  // } else {
-  //   isDeployment = true;
-  // }
+  let isDeployment = false;
+  if (isPullRequest === true) {
+    console.info('Skipping deployment commands in PRs');
+  } else if (secrets.some(secret => secret.password === undefined)) {
+    console.info(
+      'Skipping deployment commands because some secrets are not provided',
+    );
+  } else {
+    isDeployment = true;
+  }
 
   try {
     await executeCommands(
