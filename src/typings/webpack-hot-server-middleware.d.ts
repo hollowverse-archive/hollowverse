7337207496c1f051ab90dd5a9d0d778299dc565b
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-11-19 22:34:11 +0200

    Fix TSLint issues

--- src/typings/webpack-hot-server-middleware.d.ts
+++ src/typings/webpack-hot-server-middleware.d.ts
@@ -1,5 +1,6 @@
 declare module 'webpack-hot-server-middleware' {
   import { RequestHandler } from 'express';
+  // tslint:disable-next-line no-implicit-dependencies
   import { MultiCompiler, Stats } from 'webpack';
   type Options<T = Record<string, any>> = Partial<{
     /** The name of the server entry point, defaults to 'main'. */
