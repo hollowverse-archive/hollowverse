cc55071cb9d0c5ae1d27ad41086105948f5680bf
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-11-23 19:48:09 +0200

    Remove unused props on Event component

--- src/app/components/Event/Event.tsx
+++ src/app/components/Event/Event.tsx
@@ -6,21 +6,19 @@ import { Label } from 'components/Label/Label';
 import * as classes from './Event.module.scss';
 
 type EventProps = {
-  quote: string;
   postedAt: Date | null;
   happenedOn: Date | null;
   notablePerson: {
     name: string;
     photoUrl: string;
   } | null;
-  isQuoteByNotablePerson: boolean;
   sourceName: string;
   sourceUrl: string;
   labels: Array<{
     id: string;
     text: string;
   }>;
-  children?: JSX.Element | JSX.Element[] | string;
+  children?: JSX.Element | JSX.Element[] | string | null;
 };
 
 export const Event = (props: EventProps) => (
@@ -28,21 +26,23 @@ export const Event = (props: EventProps) => (
     <div className={classes.eventContent}>
       {props.children}
       <div className={classes.eventMeta}>
-        <span>
+        <div className={classes.labels}>
           {props.labels.map(label => (
             <Label key={label.id} size="small" text={label.text} />
           ))}
-        </span>
-        {props.happenedOn ? (
-          <span className={classes.eventDate}>
-            {formatDate(props.happenedOn, 'MMM D, YYYY')}
-          </span>
-        ) : null}
-        {props.sourceName ? (
-          <span className={classes.eventSource}>
-            Source: <a href={props.sourceUrl}>{props.sourceName}</a>
-          </span>
-        ) : null}
+        </div>
+        <div>
+          {props.happenedOn ? (
+            <span className={classes.eventDate}>
+              {formatDate(props.happenedOn, 'MMM D, YYYY')}
+            </span>
+          ) : null}
+          {props.sourceName ? (
+            <span className={classes.eventSource}>
+              Source: <a href={props.sourceUrl}>{props.sourceName}</a>
+            </span>
+          ) : null}
+        </div>
       </div>
     </div>
   </div>
