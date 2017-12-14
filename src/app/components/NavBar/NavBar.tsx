774d196e64d9364450460a40d825c762a25fa69c
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-12-14 15:40:58 +0200

    Add gradient effect to Hollowverse

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -3,6 +3,8 @@ import * as classes from './NavBar.module.scss';
 
 export const NavBar = (props: { title: string }) => (
   <div className={classes.navbar}>
-    <a href="/">{props.title}</a>
+    <a title={classes.title} className={classes.title} href="/">
+      {props.title}
+    </a>
   </div>
 );
