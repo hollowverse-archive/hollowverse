a7a8ddb3a2a1289b77869e85c5e0998b06fcca4e
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-12-15 20:56:30 +0200

    Add search and back buttons to navbar

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 
-import { Sticky } from 'components/Sticky/Sticky';
-
 import * as classes from './NavBar.module.scss';
+
+import { Sticky } from 'components/Sticky/Sticky';
+import { SvgIcon } from 'components/SvgIcon/SvgIcon';
 
 type Props = {
   title: string;
 };
+
+import searchIcon from 'icons/search.svg';
+import backIcon from 'icons/back.svg';
 
 export class NavBar extends React.PureComponent<Props> {
   render() {
@@ -27,9 +31,18 @@ export class NavBar extends React.PureComponent<Props> {
             </div>
           ) : (
             <div className={classes.logo}>
-              <a title={classes.title} className={classes.title} href="/">
+              <a title="Homepage" className={classes.title} href="/">
                 {title}
               </a>
+              <a className={classes.back} href="..">
+                <SvgIcon size={20} {...backIcon} />
+                <span className="sr-only">Go Back</span>
+              </a>
+              <label>
+                <input aria-checked={false} type="checkbox" />
+                <SvgIcon size={20} {...searchIcon} />
+                <span className="sr-only">Search</span>
+              </label>
             </div>
           )}
       </Sticky>
