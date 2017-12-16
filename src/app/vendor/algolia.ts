6a2e022376c125e1668321bf663dfc1c5754a2c6
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-12-16 21:40:34 +0200

    Move algolia client to vendor

---
+++ src/app/vendor/algolia.ts
@@ -0,0 +1,8 @@
+import algolia from 'algoliasearch';
+
+const SEARCH_ONLY_API_KEY = 'd970947e688348297451c41746235cd5';
+const APP_ID = '33DEXZ8MDK';
+
+export const client = algolia(APP_ID, SEARCH_ONLY_API_KEY);
+
+export const notablePeople = client.initIndex('notablePerson-dev');
