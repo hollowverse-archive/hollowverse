9015e1f4e8184d89f5fe20388a88523a4886e19a
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-12-17 23:37:10 +0200

    Link search results

--- src/app/pages/SearchResults/SearchResults.tsx
+++ src/app/pages/SearchResults/SearchResults.tsx
@@ -11,6 +11,7 @@ import { AlgoliaResponse } from 'algoliasearch';
 import { connect } from 'react-redux';
 import { getSearchQuery } from 'store/features/search/selectors';
 import { notablePeople } from 'vendor/algolia';
+import { Link } from 'react-router-dom';
 
 type ResolvedProps = {
   searchResults: AsyncResult<AlgoliaResponse | null>;
@@ -38,7 +39,7 @@ class Page extends React.PureComponent<OwnProps & ResolvedProps> {
                 {searchResults.value.hits.map(result => {
                   return (
                     <li key={result.objectID} className={classes.result}>
-                      {result.name}
+                      <Link to={`/${result.slug}`}>{result.name}</Link>
                     </li>
                   );
                 })}
