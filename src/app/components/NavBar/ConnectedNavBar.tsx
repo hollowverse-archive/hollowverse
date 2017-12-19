77c9c4990df2f2cb271b598125ffe2880451b34b
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-12-19 13:57:22 +0200

    Connect search progress for NavBar

--- src/app/components/NavBar/ConnectedNavBar.tsx
+++ src/app/components/NavBar/ConnectedNavBar.tsx
@@ -2,12 +2,16 @@ import { connect } from 'react-redux';
 import { NavBar } from './NavBar';
 import { requestSearchResults } from 'store/features/search/actions';
 import { StoreState } from 'store/types';
-import { getSearchQuery } from 'store/features/search/selectors';
+import {
+  getSearchQuery,
+  isSearchInProgress,
+} from 'store/features/search/selectors';
 
 export const ConnectedNavBar = connect(
   (state: StoreState) => ({
     searchQuery: getSearchQuery(state),
     lastSearchMatch: state.lastSearchMatch,
+    isSearchInProgress: isSearchInProgress(state),
   }),
   {
     requestSearchResults,
