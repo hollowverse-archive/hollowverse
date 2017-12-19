9ddcc71111fe1becf04653f992985e3456505a76
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-12-19 02:46:29 +0200

    Preload Notable Person page in search results

--- src/app/pages/SearchResults/LoadableSearchResults.tsx
+++ src/app/pages/SearchResults/LoadableSearchResults.tsx
@@ -1,10 +1,15 @@
 import universal from 'react-universal-component';
 import { epic$ } from 'store/store';
+import { LoadableNotablePerson } from 'pages/NotablePerson/LoadableNotablePerson';
 
 export const LoadableSearchResults = universal(import('./SearchResults'), {
   key: module => module.SearchResults,
-  onLoad({ performSearchEpic }) {
+  onLoad({ performSearchEpic }, { isServer }) {
     // Inject search epic
     epic$.next(performSearchEpic);
+
+    if (!isServer) {
+      LoadableNotablePerson.preload();
+    }
   },
 });
