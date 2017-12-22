ec06f83a4c2ada562500c3f49287f6eb0f65b735
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-12-22 15:12:51 +0200

    Prevent default navigation in NavBarLink

--- src/app/components/NavBar/NavBarButton.tsx
+++ src/app/components/NavBar/NavBarButton.tsx
@@ -3,14 +3,35 @@ import cc from 'classcat';
 
 import * as classes from './NavBarButton.module.scss';
 import { Link, LinkProps } from 'react-router-dom';
+import { withRouter, RouteComponentProps } from 'react-router';
 
 export const NavBarButton = ({
   className,
-  ...rest,
+  ...rest
 }: React.ButtonHTMLAttributes<HTMLButtonElement>) => (
   <button className={cc([className, classes.root])} {...rest} />
 );
 
-export const NavBarLink = ({ className, ...rest }: LinkProps) => (
-  <Link className={cc([className, classes.root])} {...rest} />
+export const NavBarLink = withRouter(
+  class extends React.PureComponent<LinkProps & RouteComponentProps<any>> {
+    handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+      const { to } = this.props;
+      const finalTo =
+        typeof to === 'string' ? to : this.props.history.createHref(to);
+      e.preventDefault();
+      this.props.history.push(finalTo);
+    };
+
+    render() {
+      const { className, ...rest } = this.props;
+
+      return (
+        <Link
+          className={cc([className, classes.root])}
+          {...rest}
+          onClick={this.handleClick}
+        />
+      );
+    }
+  },
 );
