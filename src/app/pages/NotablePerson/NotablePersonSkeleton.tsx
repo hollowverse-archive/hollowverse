c6d5985c2772269e83c0ab83f0dd47848d6f7fd6
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-12-23 13:10:13 +0200

    Simplify and vary length of skeleton lines

--- src/app/pages/NotablePerson/NotablePersonSkeleton.tsx
+++ src/app/pages/NotablePerson/NotablePersonSkeleton.tsx
@@ -2,15 +2,17 @@ import * as React from 'react';
 import * as classes from './NotablePersonSkeleton.module.scss';
 
 export const NotablePersonSkeleton = () => (
-  <div className={classes.root}>
+  <div className={classes.root} aria-hidden>
     <div className={classes.inner}>
       <div className={classes.gradient} />
       <div className={classes.photo} />
       <div className={classes.caption} />
       <div className={classes.name} />
-      <div className={classes.paragraph} />
-      <div className={classes.paragraph} />
-      <div className={classes.paragraph} />
+      <div className={classes.lines}>
+        <div />
+        <div />
+        <div />
+      </div>
     </div>
   </div>
 );
