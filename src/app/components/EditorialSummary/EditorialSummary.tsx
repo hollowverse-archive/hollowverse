fd932342171178757d417301b4172fb2d659f888
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-12-26 18:12:10 +0200

    Fixes to EditorialSummary

--- src/app/components/EditorialSummary/EditorialSummary.tsx
+++ src/app/components/EditorialSummary/EditorialSummary.tsx
@@ -4,7 +4,6 @@ import formatDate from 'date-fns/format';
 import * as classes from './EditorialSummary.module.scss';
 import { prettifyUrl } from 'helpers/prettifyUrl';
 import { EditorialSummaryNodeType } from 'api/types';
-import { memoize } from 'lodash';
 import { Quote } from 'components/Quote/Quote';
 
 type Node = {
@@ -26,13 +25,15 @@ const findChildren = (node: Node, nodes: Node[]) => {
   return nodes.filter(child => child.parentId === node.id);
 };
 
-const isParent = (node: Node) => {
+const isBlockNode = (node: Node) => {
   return [
     EditorialSummaryNodeType.heading,
     EditorialSummaryNodeType.paragraph,
     EditorialSummaryNodeType.quote,
   ].includes(node.type);
 };
+
+const isRootBlock = (node: Node) => !node.parentId;
 
 type Source = {
   number: number;
@@ -42,91 +43,109 @@ type Source = {
   sourceTitle: string | null;
 };
 
+type BlockProps = {
+  node: Node;
+  nodes: Node[];
+  referencesMap: Map<string | null, Source>;
+};
+
+const Block = ({ node, nodes, referencesMap }: BlockProps): JSX.Element => {
+  const children = findChildren(node, nodes).map(child => {
+    if (isBlockNode(child)) {
+      return <Block node={child} nodes={nodes} referencesMap={referencesMap} />;
+    } else if (child.type === 'link' && child.sourceUrl) {
+      return (
+        <a
+          key={child.id}
+          title={child.sourceTitle || undefined}
+          href={child.sourceUrl}
+        >
+          {child.text}
+        </a>
+      );
+    }
+
+    const ref = referencesMap.get(child.sourceUrl);
+
+    return (
+      <span key={child.id}>
+        {' '}
+        {child.type === 'emphasis' ? <em>{child.text}</em> : child.text}
+        {ref ? (
+          <a id={ref.nodeId} href={`#${ref.sourceId}`}>
+            <sup>{ref.number}</sup>
+          </a>
+        ) : null}{' '}
+      </span>
+    );
+  });
+
+  if (node.type === 'quote') {
+    return (
+      <Quote key={node.id} size="large">
+        {children}
+      </Quote>
+    );
+  } else if (node.type === 'heading') {
+    return <h3 key={node.id}>{children}</h3>;
+  }
+
+  return <p key={node.id}>{children}</p>;
+};
+
+/**
+ * Finds and adds references to `map` by performing a depth-first search
+ * for nodes with `sourceUrl`s.
+ */
+const findRefs = (nodes: Node[], map: Map<string, Source>) => (node: Node) => {
+  const { sourceUrl, sourceTitle } = node;
+  if (sourceUrl) {
+    const number = map.size + 1;
+    map.set(sourceUrl, {
+      number,
+      sourceId: `source_${number}`,
+      nodeId: `ref_${number}`,
+      sourceUrl: sourceUrl,
+      sourceTitle,
+    });
+  }
+
+  findChildren(node, nodes).forEach(findRefs(nodes, map));
+};
+
 export class EditorialSummary extends React.PureComponent<Props> {
-  // tslint:disable-next-line:max-func-body-length
+  references = new Map<string, Source>();
+
+  constructor({ nodes }: Props) {
+    super();
+
+    nodes.filter(isRootBlock).forEach(findRefs(nodes, this.references));
+  }
+
   render() {
     const { nodes, author, lastUpdatedOn } = this.props;
 
     const date = lastUpdatedOn ? new Date(lastUpdatedOn) : undefined;
 
-    const refs: Source[] = [];
-
-    const createRef = memoize((sourceUrl, { sourceTitle }: Node) => {
-      if (sourceUrl) {
-        const number = refs.length + 1;
-
-        const ref = {
-          number,
-          sourceId: `source_${number}`,
-          nodeId: `ref_${number}`,
-          sourceUrl,
-          sourceTitle,
-        };
-
-        refs.push(ref);
-
-        return ref;
-      }
-
-      return null;
-    });
-
-    const renderBlock = (node: Node): React.ReactNode => {
-      const children = findChildren(node, nodes).map(child => {
-        if (isParent(child)) {
-          return renderBlock(child);
-        } else if (child.type === 'link' && child.sourceUrl) {
-          return (
-            <a
-              key={child.id}
-              title={child.sourceTitle || undefined}
-              href={child.sourceUrl}
-            >
-              {child.text}
-            </a>
-          );
-        }
-
-        const ref = createRef(child.sourceUrl, child);
-
-        return (
-          <span key={child.id}>
-            {child.type === 'emphasis' ? <em>{child.text}</em> : child.text}
-            {ref ? (
-              <a id={ref.nodeId} href={`#${ref.sourceId}`}>
-                <sup>{ref.number}</sup>
-              </a>
-            ) : null}
-          </span>
-        );
-      });
-
-      if (node.type === 'quote') {
-        return (
-          <Quote key={node.id} size="large">
-            {children}
-          </Quote>
-        );
-      } else if (node.type === 'heading') {
-        return <h3 key={node.id}>{children}</h3>;
-      }
-
-      return <p key={node.id}>{children}</p>;
-    };
-
     return (
       <div className={classes.root}>
-        {nodes.filter(({ parentId }) => parentId === null).map(renderBlock)}
+        {nodes
+          .filter(isRootBlock)
+          .map(node => (
+            <Block node={node} nodes={nodes} referencesMap={this.references} />
+          ))}
         <hr />
         <h3>Sources</h3>
         <small>
           <ol className={classes.sourceList}>
-            {refs.map(ref => {
+            {Array.from(this.references.values()).map(ref => {
               const { nodeId, sourceId, sourceUrl, sourceTitle } = ref;
+              const prettifiedUrl = prettifyUrl(sourceUrl);
 
               return (
                 <li key={sourceUrl} id={sourceId}>
-                  <a href={sourceUrl}>{sourceTitle}</a> {prettifyUrl(sourceUrl)}
+                  <a href={sourceUrl}>{sourceTitle || prettifiedUrl}</a>
+                  {sourceTitle ? ` ${prettifiedUrl}` : null}
                   <a
                     className={classes.backLink}
                     href={`#${nodeId}`}
