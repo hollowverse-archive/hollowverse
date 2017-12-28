50da526d6bca297c65bb2b40638bccaab0d20bd1
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-12-28 22:19:28 +0200

    Do not show "loading..." when search results page is loading

--- src/app/pages/SearchResults/LoadableSearchResults.tsx
+++ src/app/pages/SearchResults/LoadableSearchResults.tsx
@@ -2,4 +2,5 @@ import universal from 'react-universal-component';
 
 export const LoadableSearchResults = universal(import('./SearchResults'), {
   key: 'SearchResults',
+  loading: () => <div />,
 });
