af4d3784a84fcd0240ed7e6935f334417ef78ef3
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-12-29 23:23:09 +0200

    Preload lazy image at 25% intersection

--- src/app/components/LazyImage/LazyImage.tsx
+++ src/app/components/LazyImage/LazyImage.tsx
@@ -1,18 +1,25 @@
 import * as React from 'react';
 
-import { OptionalIntersectionObserver } from 'components/OptionalIntersectionObserver/OptionalIntersectionObserver';
-import { Image, Props } from 'components/Image/Image';
+import {
+  OptionalIntersectionObserver,
+  Props as IntersectionObserverProps,
+} from 'components/OptionalIntersectionObserver/OptionalIntersectionObserver';
+import { Image, Props as ImageProps } from 'components/Image/Image';
+
+type Props = ImageProps & Pick<IntersectionObserverProps, 'rootMargin'>;
 
 export class LazyImage extends React.PureComponent<Props> {
   render() {
+    const { rootMargin, ...imageProps } = this.props;
+
     return (
-      <OptionalIntersectionObserver threshold={0} triggerOnce>
+      <OptionalIntersectionObserver rootMargin="0% 0% 25% 0%" triggerOnce>
         {inView => {
           if (!inView) {
             return this.props.loadingComponent;
           }
 
-          return <Image {...this.props} />;
+          return <Image {...imageProps} />;
         }}
       </OptionalIntersectionObserver>
     );
