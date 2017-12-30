03ba7851da38780d3316ac0ae65f8d054ed00758
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-12-30 15:02:25 +0200

    Use Collapsable component for Editorial Summary source list

--- src/app/components/EditorialSummary/EditorialSummary.tsx
+++ src/app/components/EditorialSummary/EditorialSummary.tsx
@@ -5,6 +5,7 @@ import * as classes from './EditorialSummary.module.scss';
 import { prettifyUrl } from 'helpers/prettifyUrl';
 import { EditorialSummaryNodeType } from 'api/types';
 import { Quote } from 'components/Quote/Quote';
+import { Collapsable } from 'components/Collapsable/Collapsable';
 
 type Node = {
   id: string;
@@ -47,12 +48,14 @@ type BlockProps = {
   node: Node;
   nodes: Node[];
   referencesMap: Map<string | null, Source>;
+  onSourceClick?: React.EventHandler<React.MouseEvent<HTMLAnchorElement>>;
 };
 
-const Block = ({ node, nodes, referencesMap }: BlockProps): JSX.Element => {
+const Block = (props: BlockProps): JSX.Element => {
+  const { node, nodes, referencesMap, onSourceClick } = props;
   const children = findChildren(node, nodes).map(child => {
     if (isBlockNode(child)) {
-      return <Block node={child} nodes={nodes} referencesMap={referencesMap} />;
+      return <Block {...props} node={child} />;
     } else if (child.type === 'link' && child.sourceUrl) {
       return (
         <a
@@ -60,7 +63,7 @@ const Block = ({ node, nodes, referencesMap }: BlockProps): JSX.Element => {
           title={child.sourceTitle || undefined}
           href={child.sourceUrl}
         >
-          {child.text} {' '}
+          {child.text}{' '}
         </a>
       );
     }
@@ -69,10 +72,9 @@ const Block = ({ node, nodes, referencesMap }: BlockProps): JSX.Element => {
 
     return (
       <span key={child.id}>
-        {child.type === 'emphasis' ? <em>{child.text}</em> : child.text}
-        {' '}
+        {child.type === 'emphasis' ? <em>{child.text}</em> : child.text}{' '}
         {ref ? (
-          <a id={ref.nodeId} href={`#${ref.sourceId}`}>
+          <a id={ref.nodeId} onClick={onSourceClick} href={`#${ref.sourceId}`}>
             <sup>{ref.number}</sup>
           </a>
         ) : null}
@@ -113,8 +115,16 @@ const findRefs = (nodes: Node[], map: Map<string, Source>) => (node: Node) => {
   findChildren(node, nodes).forEach(findRefs(nodes, map));
 };
 
-export class EditorialSummary extends React.PureComponent<Props> {
+type State = {
+  shouldShowSources: boolean;
+};
+
+export class EditorialSummary extends React.PureComponent<Props, State> {
   references = new Map<string, Source>();
+
+  state: State = {
+    shouldShowSources: __IS_SERVER__,
+  };
 
   constructor({ nodes }: Props) {
     super();
@@ -122,8 +132,13 @@ export class EditorialSummary extends React.PureComponent<Props> {
     nodes.filter(isRootBlock).forEach(findRefs(nodes, this.references));
   }
 
+  onSourceClick: BlockProps['onSourceClick'] = () => {
+    this.setState({ shouldShowSources: true });
+  };
+
   render() {
     const { nodes, author, lastUpdatedOn } = this.props;
+    const { shouldShowSources } = this.state;
 
     const date = lastUpdatedOn ? new Date(lastUpdatedOn) : undefined;
 
@@ -132,33 +147,39 @@ export class EditorialSummary extends React.PureComponent<Props> {
         {nodes
           .filter(isRootBlock)
           .map(node => (
-            <Block node={node} nodes={nodes} referencesMap={this.references} />
+            <Block
+              node={node}
+              nodes={nodes}
+              referencesMap={this.references}
+              onSourceClick={this.onSourceClick}
+            />
           ))}
         <hr />
-        <h3>Sources</h3>
-        <small>
-          <ol className={classes.sourceList}>
-            {Array.from(this.references.values()).map(ref => {
-              const { nodeId, sourceId, sourceUrl, sourceTitle } = ref;
-              const prettifiedUrl = prettifyUrl(sourceUrl);
+        <Collapsable isOpen={shouldShowSources} label={<h3>Sources</h3>}>
+          <small>
+            <ol className={classes.sourceList}>
+              {Array.from(this.references.values()).map(ref => {
+                const { nodeId, sourceId, sourceUrl, sourceTitle } = ref;
+                const prettifiedUrl = prettifyUrl(sourceUrl);
 
-              return (
-                <li key={sourceUrl} id={sourceId}>
-                  <a href={sourceUrl}>{sourceTitle || prettifiedUrl}</a>
-                  {sourceTitle ? ` ${prettifiedUrl}` : null}
-                  <a
-                    className={classes.backLink}
-                    href={`#${nodeId}`}
-                    role="button"
-                    aria-label="Go back to reference"
-                  >
-                    ↩
-                  </a>
-                </li>
-              );
-            })}
-          </ol>
-        </small>
+                return (
+                  <li key={sourceUrl} id={sourceId}>
+                    <a href={sourceUrl}>{sourceTitle || prettifiedUrl}</a>
+                    {sourceTitle ? ` ${prettifiedUrl}` : null}
+                    <a
+                      className={classes.backLink}
+                      href={`#${nodeId}`}
+                      role="button"
+                      aria-label="Go back to reference"
+                    >
+                      ↩
+                    </a>
+                  </li>
+                );
+              })}
+            </ol>
+          </small>
+        </Collapsable>
         <hr />
         <footer>
           <small>
