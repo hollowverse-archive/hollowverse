9f02314d923d94da2e9130baa90f529491750042
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-01-02 00:03:25 +0200

    Imrpove Sticky comment

--- src/app/components/Sticky/Sticky.tsx
+++ src/app/components/Sticky/Sticky.tsx
@@ -24,15 +24,15 @@ type State = {
  * 1. Makes the target element fixed.
  * 2. Creates a placeholder element to maintain the space of the target element.
  * 3. Now the two elements are laid out in the same space.
- * 4. The target element is on top because fixed creates a new stacking context.
+ * 4. The target element is on top because `fixed` creates a new stacking context.
  * 5. Now when the user scrolls, the placeholder will go out of view,
  * while the target will stay fixed at the top of the viewport.
  * 6. We monitor the placeholder element so that when it goes out of view, it means
- * that the target element has become effectively sticky from the user's point
+ * that the target element is now effectively sticking from the user's point
  * of view.
- * 7. We communicate the state changes in the render prop in case we want
- * to do something with that information, which we do in the case of `NavBar`
- * -- we want to transform the `NavBar` to `SearchView`.
+ * 7. We communicate the state changes in the render prop (`children`) in case we want
+ * to do something with that information, which we do in the case of `NavBar`:
+ * we want to transform the `NavBar` to `SearchView`.
  */
 export class Sticky extends React.PureComponent<Props, State> {
   state: State = {
