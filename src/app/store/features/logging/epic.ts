dddb87a7ac379ccc470159eacb4a28f21bffcf01
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-01-06 00:17:23 +0200

    Experiment with scheduling epics on requestIdleCallback

--- src/app/store/features/logging/epic.ts
+++ src/app/store/features/logging/epic.ts
@@ -5,30 +5,47 @@ import { Epic } from 'redux-observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/merge';
 import 'rxjs/add/operator/ignoreElements';
+import 'rxjs/add/operator/subscribeOn';
+
+import { Observable } from 'rxjs/Observable';
+import { once } from 'lodash';
+
+const getOptionalRequestIdleCallbackSchedulder = once(async () => {
+  if ('requestIdleCallback' in global) {
+    return (await import('rxjs-requestidlecallback-scheduler')).idle;
+  }
+
+  return (await import('rxjs/scheduler/async')).async;
+});
 
 export const loggingEpic: Epic<Action, StoreState> = action$ => {
-  return action$
-    .ofType('LOG')
-    .do(async action => {
-      try {
-        await fetch('/log', {
-          method: 'POST',
-          body: JSON.stringify(action.payload),
+  return Observable.fromPromise(
+    getOptionalRequestIdleCallbackSchedulder(),
+  ).mergeMap(scheduler =>
+    action$
+      .ofType('LOG')
+      .subscribeOn(scheduler)
+      .do(async action => {
+        try {
+          await fetch('/log', {
+            method: 'POST',
+            body: JSON.stringify(action.payload),
 
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
 
-          // We need to include the `branch` and `env`
-          // cookies to route the log request to the
-          // logging endpoint that corresponds to this
-          // branch.
-          credentials: 'include',
-        });
-      } catch (e) {
-        console.error('Failed to send log event', e);
-      }
-    })
-    .ignoreElements();
+            // We need to include the `branch` and `env`
+            // cookies to route the log request to the
+            // logging endpoint that corresponds to this
+            // branch.
+            credentials: 'include',
+          });
+        } catch (e) {
+          console.error('Failed to send log event', e);
+        }
+      })
+      .ignoreElements(),
+  );
 };
