a47d54ae244f2ba14ec503ca8f756d08024576ea
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-01-06 12:34:41 +0200

    Remove Loggable

--- src/app/components/LoggableLink/LoggableLink.tsx
+++ src/app/components/LoggableLink/LoggableLink.tsx
@@ -1,33 +1,28 @@
 import * as React from 'react';
-import { Loggable } from 'components/Loggable/Loggable';
 import { Link, LinkProps } from 'react-router-dom';
 import { callAll } from 'helpers/callAll';
 import { createPath } from 'history';
+import { connect } from 'react-redux';
+import { log as logAction } from 'store/features/logging/actions';
 
 export type LoggableLinkProps = LinkProps & {
   shouldLogClick?: boolean;
+  log: typeof logAction;
 };
 
-export const LoggableLink = ({
-  onClick,
-  shouldLogClick = true,
-  to,
-  ...rest
-}: LoggableLinkProps) => (
-  <Loggable>
-    {({ log }) => {
-      const logClick = () =>
-        log('PAGE_REQUESTED', {
-          url: typeof to === 'string' ? to : createPath(to),
-        });
+export const LoggableLink = connect(undefined, { log: logAction })(
+  ({ onClick, shouldLogClick = true, to, log, ...rest }: LoggableLinkProps) => {
+    const logClick = () =>
+      log('PAGE_REQUESTED', {
+        url: typeof to === 'string' ? to : createPath(to),
+      });
 
-      return (
-        <Link
-          to={to}
-          onClick={callAll(onClick, shouldLogClick ? logClick : undefined)}
-          {...rest}
-        />
-      );
-    }}
-  </Loggable>
+    return (
+      <Link
+        to={to}
+        onClick={callAll(onClick, shouldLogClick ? logClick : undefined)}
+        {...rest}
+      />
+    );
+  },
 );
