33e92d1b238055bcb95c8fd50b1b694040eea575
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-01-06 18:58:01 +0200

    Document global variables

--- src/app/typings/global.d.ts
+++ src/app/typings/global.d.ts
@@ -1,8 +1,26 @@
+/** Whether the `DEBUG` flag has been passed to the CLI start command */
 declare const __IS_DEBUG__: boolean;
+
+/**
+ * `true` if the code is running in Node.js (server-side rendering),
+ * `false` otherwise
+ */
 declare const __IS_SERVER__: boolean;
+
+/** Git branch of the current build, or `undefined` for development */
 declare const __BRANCH__: string | undefined;
+
+/** Git commit hash of the current build, or `undefined` for development */
 declare const __COMMIT_ID__: string | undefined;
+
+/**
+ * The base URL of the current app server,
+ * i.e. https://hollowvese.com for production or
+ * http://localhost:3001 for development
+ */
 declare const __BASE__: string;
+
+/** @example https://api.hollowverse.com/graphql */
 declare const __API_ENDPOINT__: string;
 
 declare const FB: facebookSdk.Fb;
