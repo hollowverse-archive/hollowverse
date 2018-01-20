42d759e815025243e4389fecc47fa771c94873e3
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-01-20 21:11:40 +0200

    Explain how file-loader in loadPolyfill works

--- src/app/helpers/loadPolyfill.ts
+++ src/app/helpers/loadPolyfill.ts
@@ -1,5 +1,10 @@
 import { importGlobalScript } from 'helpers/importGlobalScript';
 
+// `file-loader` insructs Webpack to output the imported module to a separate
+// file, and returns the runtime URL of that file so that we can import it
+// as a global script.
+// Polyfills must be imported as global scripts because they need to access things
+// like `window` and `global`.
 import intersectionObserverPolyfillUrl from 'file-loader!intersection-observer';
 import fetchPolyfillUrl from 'file-loader!whatwg-fetch';
 
