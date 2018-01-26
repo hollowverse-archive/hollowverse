867fb8a86aff1d4c7a8f4b977ff3c9f33dfefc8a
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-01-26 20:43:03 +0200

    Memoize API query result on success

--- src/isNewSlug.ts
+++ src/isNewSlug.ts
@@ -10,8 +10,26 @@ const query = `
   }
 `;
 
-export const isNewSlug = async (path: string) => {
+const memoizeOnSuccess = <T extends string, R>(
+  fn: ((arg: T) => Promise<R>),
+) => {
+  const map = new Map<string, R>();
+
+  return async (arg: T) => {
+    const cachedResult = map.get(arg);
+    if (cachedResult !== undefined) {
+      return cachedResult;
+    }
+
+    const result = await fn(arg);
+    map.set(arg, result);
+
+    return result;
+  };
+};
+
+export const isNewSlug = memoizeOnSuccess(async (path: string) => {
   const result = await client.request<any>(query, { slug: path });
 
   return result.notablePerson !== null;
-};
+});
