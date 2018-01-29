2f575c8f6e2a9bdef20927be3f6ea87eebfb255d
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-01-29 20:35:28 +0200

    Refactor

--- src/app/server.tsx
+++ src/app/server.tsx
@@ -35,10 +35,10 @@ type IconStats = {
 };
 
 import { logEndpoint } from './logger/logEndpoint';
-import { redirectionMap } from 'redirectionMap';
-import { isNewSlug } from 'isNewSlug';
+import { redirectionMap } from './redirectionMap';
+import { isNewSlug } from './isNewSlug';
+import { RequestHandler } from 'express-serve-static-core';
 
-// tslint:disable-next-line:max-func-body-length
 export const createServerRenderMiddleware = ({
   clientStats,
   iconStats,
@@ -46,12 +46,119 @@ export const createServerRenderMiddleware = ({
   clientStats: Stats;
   iconStats: IconStats | undefined;
 }) => {
-  const middleware = express();
-
-  middleware.use('/log', logEndpoint);
-
   // tslint:disable-next-line:max-func-body-length
-  middleware.use(async (req, res, next) => {
+  const serverRenderMiddleware: RequestHandler = async (req, res) => {
+    const start = Date.now();
+    const history = createMemoryHistory({ initialEntries: [req.url] });
+    const { store, wrappedRootEpic } = createConfiguredStore(
+      history,
+      undefined,
+      wrapRootEpic,
+    );
+
+    const helmetContext = {};
+
+    renderToString(
+      <HelmetProvider context={helmetContext}>
+        <Provider store={store}>
+          <ConnectedRouter history={history}>
+            <App />
+          </ConnectedRouter>
+        </Provider>
+      </HelmetProvider>,
+      wrappedRootEpic,
+    ).subscribe({
+      next({ markup }) {
+        const state = store.getState();
+        logger.debug('Server-side Redux state:', state);
+
+        const statusCode = getStatusCode(state) || 200;
+        res.status(statusCode);
+
+        if (statusCode === 301 || statusCode === 302) {
+          const url = getRedirectionUrl(state) as string;
+          res.redirect(url);
+        } else {
+          const chunkNames = flushChunkNames();
+
+          const {
+            js,
+            styles,
+            cssHash,
+            scripts,
+            stylesheets,
+            publicPath,
+          } = flushChunks(clientStats, { chunkNames });
+
+          logger.debug(`Request path: ${req.path}`);
+          logger.debug('Dynamic chunk names rendered', chunkNames);
+          logger.debug('Scripts served:', scripts);
+          logger.debug('Stylesheets served:', stylesheets);
+          logger.debug('Icon stats:', iconStats);
+          logger.debug('Public path:', publicPath);
+
+          // Tell browsers to start fetching scripts and stylesheets as soon as they
+          // parse the HTTP headers of the page
+          res.setHeader(
+            'Link',
+            [
+              ...stylesheets.map(
+                src => `<${publicPath}/${src}>; rel=preload; as=style`,
+              ),
+              ...scripts.map(
+                src => `<${publicPath}/${src}>; rel=preload; as=script`,
+              ),
+            ].join(','),
+          );
+
+          const icons = iconStats ? iconStats.html.join(' ') : '';
+
+          const { title, meta, link, htmlAttributes } = mapValues(
+            (helmetContext as FilledContext).helmet,
+            String,
+          );
+
+          res.send(
+            interpolateTemplate({
+              // In order to protect from XSS attacks, make sure to use `serialize-javascript`
+              // to serialize all data. `JSON.stringify` won't protect from XSS.
+              // If `data` contains "</script><script>alert('Haha! Pwned!')</script>",
+              // `JSON.stringify` won't help.
+              initialState: serializeJavaScript(state, {
+                isJSON: true,
+                space: __IS_DEBUG__ ? 2 : 0,
+              }),
+              markup,
+              icons,
+              js,
+              styles,
+              cssHash,
+              htmlAttributes,
+              link,
+              title,
+              meta,
+            }),
+          );
+        }
+      },
+
+      complete() {
+        const end = Date.now();
+        logger.debug(`Request took ${end - start}ms to process`);
+      },
+
+      error(error: Error) {
+        logger.error(error);
+        res.status(500).send('Error');
+      },
+    });
+  };
+
+  const entryMiddleware = express();
+
+  entryMiddleware.use('/log', logEndpoint);
+
+  entryMiddleware.use(async (req, res, next) => {
     try {
       // `req.url` matches: /Tom_Hanks, /tom-hanks, /app.js, /michael-jackson, ashton-kutcher...
       const path: string = req.url.replace('/', '');
@@ -59,125 +166,16 @@ export const createServerRenderMiddleware = ({
       if (redirectionPath !== undefined) {
         // /tom-hanks => redirect to Tom_Hanks
         res.redirect(`/${redirectionPath}`);
-
-        return;
       } else if (!await isNewSlug(path)) {
         next();
-
-        return;
+      } else {
+        serverRenderMiddleware(req, res, next);
       }
-
-      const start = Date.now();
-      const history = createMemoryHistory({ initialEntries: [req.url] });
-      const { store, wrappedRootEpic } = createConfiguredStore(
-        history,
-        undefined,
-        wrapRootEpic,
-      );
-
-      const helmetContext = {};
-
-      renderToString(
-        <HelmetProvider context={helmetContext}>
-          <Provider store={store}>
-            <ConnectedRouter history={history}>
-              <App />
-            </ConnectedRouter>
-          </Provider>
-        </HelmetProvider>,
-        wrappedRootEpic,
-      ).subscribe({
-        next({ markup }) {
-          const state = store.getState();
-          logger.debug('Server-side Redux state:', state);
-
-          const statusCode = getStatusCode(state) || 200;
-          res.status(statusCode);
-
-          if (statusCode === 301 || statusCode === 302) {
-            const url = getRedirectionUrl(state) as string;
-            res.redirect(url);
-
-            return;
-          } else {
-            const chunkNames = flushChunkNames();
-
-            const {
-              js,
-              styles,
-              cssHash,
-              scripts,
-              stylesheets,
-              publicPath,
-            } = flushChunks(clientStats, { chunkNames });
-
-            logger.debug(`Request path: ${req.path}`);
-            logger.debug('Dynamic chunk names rendered', chunkNames);
-            logger.debug('Scripts served:', scripts);
-            logger.debug('Stylesheets served:', stylesheets);
-            logger.debug('Icon stats:', iconStats);
-            logger.debug('Public path:', publicPath);
-
-            // Tell browsers to start fetching scripts and stylesheets as soon as they
-            // parse the HTTP headers of the page
-            res.setHeader(
-              'Link',
-              [
-                ...stylesheets.map(
-                  src => `<${publicPath}/${src}>; rel=preload; as=style`,
-                ),
-                ...scripts.map(
-                  src => `<${publicPath}/${src}>; rel=preload; as=script`,
-                ),
-              ].join(','),
-            );
-
-            const icons = iconStats ? iconStats.html.join(' ') : '';
-
-            const { title, meta, link, htmlAttributes } = mapValues(
-              (helmetContext as FilledContext).helmet,
-              String,
-            );
-
-            res.send(
-              interpolateTemplate({
-                // In order to protect from XSS attacks, make sure to use `serialize-javascript`
-                // to serialize all data. `JSON.stringify` won't protect from XSS.
-                // If `data` contains "</script><script>alert('Haha! Pwned!')</script>",
-                // `JSON.stringify` won't help.
-                initialState: serializeJavaScript(state, {
-                  isJSON: true,
-                  space: __IS_DEBUG__ ? 2 : 0,
-                }),
-                markup,
-                icons,
-                js,
-                styles,
-                cssHash,
-                htmlAttributes,
-                link,
-                title,
-                meta,
-              }),
-            );
-          }
-        },
-
-        complete() {
-          const end = Date.now();
-          logger.debug(`Request took ${end - start}ms to process`);
-        },
-
-        error(error: Error) {
-          logger.error(error);
-          res.status(500).send('Error');
-        },
-      });
     } catch (e) {
       console.error(e);
       next();
     }
   });
 
-  return middleware;
+  return entryMiddleware;
 };
