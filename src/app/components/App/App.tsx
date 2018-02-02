20da52ca86f8e5f298aedbebf73888e8d4ee442f
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-02-02 15:55:42 +0200

    Add crossOrigin attr

--- src/app/components/App/App.tsx
+++ src/app/components/App/App.tsx
@@ -45,7 +45,7 @@ export const App = class extends React.Component<{}, State> {
             content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, shrink-to-fit=no"
           />
           <meta charSet="utf-8" />
-          <link rel="preconnect" href={__API_ENDPOINT__} />
+          <link rel="preconnect" href={__API_ENDPOINT__} crossOrigin="anonymous" />
         </Helmet>
         <Route>
           {props => {
