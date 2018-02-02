3cee88eae249e200deaeb681a20cf786aef37228
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-02-02 17:40:11 +0200

    Use as many available colors as possible

--- src/app/components/PersonDetails/PersonDetails.tsx
+++ src/app/components/PersonDetails/PersonDetails.tsx
@@ -3,7 +3,7 @@ import { Label } from 'components/Label/Label';
 import { Image } from 'components/Image/Image';
 import * as classes from './PersonDetails.module.scss';
 import { prettifyUrl } from 'helpers/prettifyUrl';
-import CustomProperties from 'react-custom-properties';
+import Helmet from 'react-helmet-async';
 
 type PersonDetailsProps = {
   summary: string | null;
@@ -13,9 +13,11 @@ type PersonDetailsProps = {
     sourceUrl: string;
     colorPalette: {
       vibrant: string | null;
+      lightVibrant: string | null;
       darkVibrant: string | null;
       muted: string | null;
       lightMuted: string | null;
+      darkMuted: string | null;
     } | null;
   } | null;
   labels?: Array<{ text: string; id: string }>;
@@ -26,56 +28,78 @@ export const PersonDetails = ({
   name,
   photo,
   labels,
-}: PersonDetailsProps) => (
-  <div className={classes.root}>
-    {photo && photo.colorPalette && photo.colorPalette.vibrant !== null ? (
-      <CustomProperties
-        // @ts-ignore
-        properties={{
-          '--color-cover-primary-dark': photo.colorPalette.darkVibrant,
-          '--color-cover-primary': photo.colorPalette.vibrant,
-          '--color-cover-secondary': photo.colorPalette.muted,
-          '--color-cover-secondary-light': photo.colorPalette.lightMuted,
-        }}
-      >
-        <div className={classes.coverPhotoWrapper} aria-hidden>
-          <div className={classes.coverPhoto} />
+}: PersonDetailsProps) => {
+  let colors: string[] = [];
+  if (photo && photo.colorPalette) {
+    colors = [
+      photo.colorPalette.darkVibrant,
+      photo.colorPalette.vibrant,
+      photo.colorPalette.lightVibrant,
+      photo.colorPalette.darkMuted,
+      photo.colorPalette.muted,
+      photo.colorPalette.lightMuted,
+    ].filter(color => color != null) as string[];
+  }
+
+  return (
+    <div className={classes.root}>
+      {photo &&
+      photo.colorPalette &&
+      photo.colorPalette.darkVibrant !== null ? (
+        <Helmet>
+          <meta name="theme-color" content={photo.colorPalette.darkVibrant} />
+        </Helmet>
+      ) : null}
+      <div className={classes.coverPhotoWrapper} aria-hidden>
+        <div
+          className={classes.coverPhoto}
+          style={
+            colors.length >= 4
+              ? {
+                  background: `linear-gradient(
+              130deg,
+              ${colors[0]} -20%,
+              ${colors[1]} 30%,
+              ${colors[2]} 60%,
+              ${colors[3]} 120%
+            )`,
+                }
+              : undefined
+          }
+        />
+      </div>
+      {photo ? (
+        <a
+          className={classes.photoLink}
+          href={photo.sourceUrl}
+          title={`Image source: ${prettifyUrl(photo.sourceUrl)}`}
+        >
+          <Image className={classes.photo} src={photo.url} alt={name} />
+          <span className="sr-only">
+            Image source: {prettifyUrl(photo.sourceUrl)}
+          </span>
+        </a>
+      ) : null}
+      <h1 className={classes.name}>
+        <div className={classes.caption}>Religion, politics, and ideas of</div>
+        {name}
+      </h1>
+      {labels && labels.length > 0 ? (
+        <ul aria-label="Labels" className={classes.labels}>
+          {labels.map(({ id, text }) => (
+            <li className={classes.listItem} key={id}>
+              <Label text={text} />
+            </li>
+          ))}
+        </ul>
+      ) : null}
+      {summary && (
+        <div className={classes.summary}>
+          {summary
+            .split('\n')
+            .map((paragraph, i) => <p key={i}>{paragraph}</p>)}
         </div>
-      </CustomProperties>
-    ) : null}
-    {photo ? (
-      <a
-        className={classes.photoLink}
-        href={photo.sourceUrl}
-        title={`Image source: ${prettifyUrl(photo.sourceUrl)}`}
-      >
-        <Image className={classes.photo} src={photo.url} alt={name} />
-        <span className="sr-only">
-          Image source: {prettifyUrl(photo.sourceUrl)}
-        </span>
-      </a>
-    ) : null}
-    <h1 className={classes.name}>
-      <div className={classes.caption}>
-        Religion, politics, and ideas of
-      </div>
-      {name}
-    </h1>
-    {labels && labels.length > 0 ? (
-      <ul aria-label="Labels" className={classes.labels}>
-        {labels.map(({ id, text }) => (
-          <li className={classes.listItem} key={id}>
-            <Label text={text} />
-          </li>
-        ))}
-      </ul>
-    ) : null}
-    {summary && (
-      <div className={classes.summary}>
-        {summary
-          .split('\n')
-          .map((paragraph, i) => <p key={i}>{paragraph}</p>)}
-      </div>
-    )}
-  </div>
-);
+      )}
+    </div>
+  );
+};
