001e43bfeb1c96f9a14cb84e262b2ac5fc5cab0d
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-02-09 20:17:39 +0200

    Default to REACT

--- src/webpack/webpack.config.test.js
+++ src/webpack/webpack.config.test.js
@@ -44,7 +44,35 @@ const testSpecificConfig = {
   
   performance: false,
 
-  externals: [nodeExternals()],
+  // By default, webpack will consume and bundle all `require` calls.
+  // `externals` specifies which packages should *not* be bundled by webpack.
+  // The following packages are already installed on the server, so they do
+  // not need to be bundled. This also reduces the build time for the server bundle.
+  // The `webpack-node-externals` package will exclude all packages in `node_modules`
+  // so they are not bundled.
+  externals: nodeExternals({
+    // `whitelist` excludes node modules so they _are_ bundled with webpack
+    whitelist: [
+      '.bin',
+      'babel-polyfill',
+
+      // @ts-ignore
+      moduleName =>
+        [
+          // These packages need to be bundled so that they
+          // know they are running in the context of webpack runtime
+          'babel-plugin-universal-import',
+          'webpack-flush-chunks',
+          'react-universal-component',
+
+          // All aliased packages should be bundled.
+          // Example: when using preact instead of React, require('react') should be bundled.
+          // Otherwise, the call to require('react') will resolve to the actual
+          // `react` package
+          ...Object.keys(common.resolve.alias),
+        ].some(match => moduleName.includes(match)),
+    ],
+  }),
   
   module: {
     rules: compact([
