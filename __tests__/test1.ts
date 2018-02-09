bab7674f8308a06a104fe2b7014e43682b53c75e
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-02-09 15:55:24 +0200

    Test that assertions work

---
+++ __tests__/test1.ts
@@ -0,0 +1,11 @@
+import 'expect-more-jest';
+
+describe('Testing tests', () => {
+  it('should work', () => {
+    expect(1).toBe(2);
+  });
+
+  it('should not work', () => {
+    expect(1).toBe(2);
+  });
+});
--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   globals: {
     'ts-jest': {
-      skipBabel: true,
+      skipBabel: false,
     },
   },
   transform: {
