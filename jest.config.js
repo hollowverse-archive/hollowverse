80d1d7efff136decd90713e5a4c96f880aa0c2e0
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-02-09 15:30:20 +0200

    Add Jest config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  globals: {
+    'ts-jest': {
+      skipBabel: true,
+    },
+  },
+  transform: {
+    '^.+\\.tsx?$': '<rootDir>/node_modules/ts-jest/preprocessor.js',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  testPathIgnorePatterns: ['__snapshots__'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+};
