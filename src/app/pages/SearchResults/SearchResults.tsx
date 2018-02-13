735f6aedbb25d6bf327ad084846cc7914240becd
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-02-13 15:04:16 +0200

    Fix TSLint issues

--- src/app/pages/SearchResults/SearchResults.tsx
+++ src/app/pages/SearchResults/SearchResults.tsx
@@ -35,97 +35,103 @@ type Props = {
   searchResultSelected(path: string): any;
 };
 
-const Page = withRouter(class extends React.PureComponent<Props & RouteComponentProps<any>> {
-  load = async (): Promise<null | AlgoliaResponse> => {
-    const { searchQuery } = this.props;
+const Page = withRouter(
+  class extends React.PureComponent<Props & RouteComponentProps<any>> {
+    load = async (): Promise<null | AlgoliaResponse> => {
+      const { searchQuery } = this.props;
 
-    if (searchQuery) {
-      return import('vendor/algolia').then(({ notablePeople }) =>
-        notablePeople.search(searchQuery),
-      );
-    }
+      if (searchQuery) {
+        return import('vendor/algolia').then(async ({ notablePeople }) =>
+          notablePeople.search(searchQuery),
+        );
+      }
 
-    return null;
-  };
+      return null;
+    };
 
-  render() {
-    const { searchQuery, location } = this.props;
+    render() {
+      const { searchQuery, location } = this.props;
 
-    return (
-      <div className={classes.root}>
-        <Helmet>
-          <title>Search</title>
-        </Helmet>
-        <div className={classes.resultsContainer}>
-          <WithData
-            requestId={searchQuery}
-            dataKey="searchResults"
-            load={this.load}
-            allowOptimisticUpdates
-          >
-            {({ result }: { result: AsyncResult<AlgoliaResponse | null> }) => {
-              if (isSuccessResult(result) || isOptimisticResult(result)) {
-                const value = result.value;
+      return (
+        <div className={classes.root}>
+          <Helmet>
+            <title>Search</title>
+          </Helmet>
+          <div className={classes.resultsContainer}>
+            <WithData
+              requestId={searchQuery}
+              dataKey="searchResults"
+              load={this.load}
+              allowOptimisticUpdates
+            >
+              {({
+                result,
+              }: {
+                result: AsyncResult<AlgoliaResponse | null>;
+              }) => {
+                if (isSuccessResult(result) || isOptimisticResult(result)) {
+                  const value = result.value;
 
-                // User just landed on search page, page is empty
-                if (!searchQuery || !value) {
-                  return <Status code={200} />;
-                }
+                  // User just landed on search page, page is empty
+                  if (!searchQuery || !value) {
+                    return <Status code={200} />;
+                  }
 
-                if (value.hits.length === 0) {
+                  if (value.hits.length === 0) {
+                    return (
+                      <MessageWithIcon
+                        className={classes.placeholder}
+                        icon={<SvgIcon {...searchIcon} />}
+                        title="No results found"
+                      >
+                        <Status key={searchQuery} code={404} />
+                      </MessageWithIcon>
+                    );
+                  }
+
                   return (
-                    <MessageWithIcon
-                      className={classes.placeholder}
-                      icon={<SvgIcon {...searchIcon} />}
-                      title="No results found"
-                    >
-                      <Status key={searchQuery} code={404} />
-                    </MessageWithIcon>
+                    <div>
+                      <Card className={classes.card}>
+                        <ResultsList
+                          hits={value.hits}
+                          onResultClick={this.props.searchResultSelected}
+                        />
+                        <Status key={searchQuery} code={200} />
+                      </Card>
+                    </div>
                   );
                 }
 
+                if (isPendingResult(result)) {
+                  return <SearchResultsSkeleton />;
+                }
+
                 return (
-                  <div>
-                    <Card className={classes.card}>
-                      <ResultsList
-                        hits={value.hits}
-                        onResultClick={this.props.searchResultSelected}
-                      />
-                      <Status key={searchQuery} code={200} />
-                    </Card>
-                  </div>
+                  <MessageWithIcon
+                    className={classes.placeholder}
+                    icon={<SvgIcon {...searchIcon} />}
+                    title="Failed to load search results"
+                    button={
+                      <LinkButton to={location} onClick={forceReload}>
+                        Reload
+                      </LinkButton>
+                    }
+                  >
+                    <Status code={500} />
+                  </MessageWithIcon>
                 );
-              }
-
-              if (isPendingResult(result)) {
-                return <SearchResultsSkeleton />;
-              }
-
-              return (
-                <MessageWithIcon
-                  className={classes.placeholder}
-                  icon={<SvgIcon {...searchIcon} />}
-                  title="Failed to load search results"
-                  button={
-                    <LinkButton to={location} onClick={forceReload}>
-                      Reload
-                    </LinkButton>
-                  }
-                >
-                  <Status code={500} />
-                </MessageWithIcon>
-              );
-            }}
-          </WithData>
+              }}
+            </WithData>
+          </div>
+          <small className={classes.algoliaContainer}>
+            Search powered by
+            <img className={classes.logo} src={algoliaLogo} alt="Algolia" />
+          </small>
         </div>
-        <small className={classes.algoliaContainer}>
-          Search powered by
-          <img className={classes.logo} src={algoliaLogo} alt="Algolia" />
-        </small>
-      </div>
-    );
-  }
-});
+      );
+    }
+  },
+);
 
 export const SearchResults = connect(
   (state: StoreState) => ({
