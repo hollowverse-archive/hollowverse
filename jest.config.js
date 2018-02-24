a515ecd1db662ad726add3043e63340fc380824c
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-02-24 21:29:36 +0200

    Extract `testsPattern` constant in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 const { testsDistDirectory } = require('./src/webpack/variables');
 
+const testsPattern = '**/tests/**/*.js';
+
 module.exports = {
   roots: [testsDistDirectory],
-  testMatch: ['**/tests/**/*.js'],
+  testMatch: [testsPattern],
   transform: {
     // Must be an empty object to prevent jest from using babel to
     // transform the files (we are already using babel via webpack)
@@ -14,6 +16,6 @@ module.exports = {
   // Jest will exclude test files from code coverage by default,
   // this will force Jest to collect coverage from the compiled
   // test files so that we can map them to the source files.
-  forceCoverageMatch: ['__tests__/**/*.js', '**/tests/**/*.js'],
-  collectCoverageFrom: ['__tests__/**/*.js', '!**/node_modules/**'],
+  forceCoverageMatch: [testsPattern],
+  collectCoverageFrom: [testsDistDirectory, '!**/node_modules/**'],
 };
