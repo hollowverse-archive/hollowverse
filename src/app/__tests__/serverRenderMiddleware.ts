52b6154beeacc0de23e31164bfab85650ae60532
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-02-24 19:35:41 +0200

    Refactor server-side tests using new helpers, add more tests for pages

--- src/app/__tests__/serverRenderMiddleware.ts
+++ src/app/__tests__/serverRenderMiddleware.ts
@@ -1,26 +1,143 @@
-import { createServerRenderMiddleware } from 'createServerRenderMiddleware';
-import supertest from 'supertest';
 import {
-  defaultTestDependencyOverrides,
-  testRoutesMap,
-} from 'helpers/testHelpers';
-import express from 'express';
+  createServerSideTestContext,
+  ServerSideTestContext,
+} from 'helpers/serverTestHelpers';
+import { createMockGetResponseForDataRequest } from 'helpers/testHelpers';
+import { stubNotablePersonQueryResponse } from 'fixtures/notablePersonQuery';
+import {
+  stubNonEmptySearchResults,
+  emptySearchResults,
+} from 'fixtures/searchResults';
 
 describe('Server rendering middleware', () => {
-  it('works', async () => {
-    const app = express();
-    const ssrMiddleware = createServerRenderMiddleware({
-      // @ts-ignore
-      clientStats: __non_webpack_require__('.././bundle/stats.json'),
-      epicDependenciesOverrides: defaultTestDependencyOverrides,
-      routesMap: testRoutesMap,
+  let context: ServerSideTestContext;
+
+  describe('Notable Person page', () => {
+    beforeEach(async () => {
+      context = await createServerSideTestContext({
+        path: '/Tom_Hanks',
+        epicDependenciesOverrides: {
+          getResponseForDataRequest: createMockGetResponseForDataRequest(
+            'notablePersonQuery',
+            stubNotablePersonQueryResponse,
+          ),
+        },
+      });
     });
 
-    app.use(ssrMiddleware);
+    describe('When notable person is found', () => {
+      it('returns 200', () => {
+        expect(context.res.status).toBe(200);
+      });
 
-    const res = await supertest(app).get('/');
+      it('has body', () => {
+        expect(context.res.text).toBeNonEmptyString();
+      });
 
-    expect(res.status).toBe(200);
-    expect(res.body).toBeDefined();
+      it('has notable person name', () => {
+        expect(
+          context
+            .$('body')
+            .find('h1')
+            .text(),
+        ).toContain('Tom Hanks');
+      });
+
+      it('shows related people', () => {
+        expect(context.$('body').text()).toContain('Al Pacino');
+      });
+    });
+
+    describe('When notable person is not found', () => {
+      beforeEach(async () => {
+        context = await createServerSideTestContext({
+          path: '/Tom_Hanks',
+          epicDependenciesOverrides: {
+            getResponseForDataRequest: createMockGetResponseForDataRequest(
+              'notablePersonQuery',
+              {
+                notablePerson: null,
+              },
+            ),
+          },
+        });
+      });
+
+      it('returns 404', () => {
+        expect(context.res.status).toBe(404);
+      });
+
+      it('has body', () => {
+        expect(context.res.text).toBeNonEmptyString();
+      });
+
+      it('shows "Not Found"', () => {
+        expect(context.$('body').text()).toContain('Not Found');
+      });
+    });
+  });
+
+  describe('Search page', () => {
+    describe('When results have finished loading,', () => {
+      beforeEach(async () => {
+        context = await createServerSideTestContext({
+          path: '/search?query=Tom',
+          epicDependenciesOverrides: {
+            getResponseForDataRequest: createMockGetResponseForDataRequest(
+              'searchResults',
+              stubNonEmptySearchResults,
+            ),
+          },
+        });
+      });
+
+      describe('When results are found,', () => {
+        it('returns 200', () => {
+          expect(context.res.status).toBe(200);
+        });
+
+        it('shows a list of results', () => {
+          expect(context.$('body').text()).toContain('Tom Hanks');
+          expect(context.$('body').text()).toContain('Tom Hardy');
+        });
+
+        it('results link to the respective notable person page', () => {
+          context
+            .$('body')
+            .find('li')
+            .each((_, li) => {
+              const $li = context.$(li);
+              for (const result of stubNonEmptySearchResults.hits) {
+                if ($li.text().includes(result.name)) {
+                  const $a = $li.find('a');
+                  expect($a.attr('href')).toContain(result.slug);
+                }
+              }
+            });
+        });
+      });
+
+      describe('When no results are found,', () => {
+        beforeEach(async () => {
+          context = await createServerSideTestContext({
+            path: '/search?query=Tom',
+            epicDependenciesOverrides: {
+              getResponseForDataRequest: createMockGetResponseForDataRequest(
+                'searchResults',
+                emptySearchResults,
+              ),
+            },
+          });
+        });
+
+        it('returns 404', () => {
+          expect(context.res.status).toBe(404);
+        });
+
+        it('shows "No results found"', () => {
+          expect(context.$('body').text()).toContain('No results found');
+        });
+      });
+    });
   });
 });
