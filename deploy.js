a99723df301a672f67a7c66c63f84c4309df6797
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-03-04 02:01:45 +0200

    Update @hollowverse/common and @hollowverse/validate-filenames (#329)
    Also move `@hollowverse/common` from `dependencies` to `devDependencies`

--- deploy.js
+++ deploy.js
@@ -4,10 +4,16 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 
 const shelljs = require('shelljs');
-const decryptSecrets = require('@hollowverse/common/helpers/decryptSecrets');
-const executeCommand = require('@hollowverse/common/helpers/executeCommand');
-const executeCommands = require('@hollowverse/common/helpers/executeCommands');
-const createZipFile = require('@hollowverse/common/helpers/createZipFile');
+const {
+  decryptSecrets,
+} = require('@hollowverse/common/helpers/decryptSecrets');
+const {
+  executeCommand,
+} = require('@hollowverse/common/helpers/executeCommand');
+const {
+  executeCommands,
+} = require('@hollowverse/common/helpers/executeCommands');
+const { createZipFile } = require('@hollowverse/common/helpers/createZipFile');
 
 const { ENC_PASS_SUMO, IS_PULL_REQUEST, PROJECT, BRANCH } = shelljs.env;
 
