ea4dd759c84f67e6c9e953e1d2fb154f312b455d
wholesomedev <mk@hollowverse.com>
2018-03-05 12:00:48 -0800

    Implement skeleton as part of original markup (#331)

--- src/app/pages/NotablePerson/LoadableNotablePerson.tsx
+++ src/app/pages/NotablePerson/LoadableNotablePerson.tsx
@@ -1,11 +1,11 @@
 import universal from 'react-universal-component';
 import { Props } from './NotablePerson';
-import { NotablePersonSkeleton } from './NotablePersonSkeleton';
+import { NotablePersonBody } from './NotablePersonBody';
 
 export const LoadableNotablePerson = universal<Props>(
   import('./NotablePerson'),
   {
     key: module => module.NotablePerson,
-    loading: NotablePersonSkeleton,
+    loading: NotablePersonBody,
   },
 );
