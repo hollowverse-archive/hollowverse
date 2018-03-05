1be84d3b167ae611bfa5fa6da04abd310ab60af7
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-03-05 22:13:44 +0200

    Use never instead of any in false branch of ArrayElement type

--- src/app/typings/typeHelpers.d.ts
+++ src/app/typings/typeHelpers.d.ts
@@ -1,3 +1,3 @@
 type UnboxPromise<T> = T extends Promise<infer R> ? R : T;
 
-type ArrayElement<T extends any[]> = T extends Array<infer R> ? R : any;
+type ArrayElement<T extends any[]> = T extends Array<infer R> ? R : never;
