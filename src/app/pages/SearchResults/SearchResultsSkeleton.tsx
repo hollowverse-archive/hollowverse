2fe99a8540f5be2d3e49f9f9c5dd292e88e60760
wholesomedev <msafi+hollowverse@msafi.com>
2018-03-11 16:31:50 -0700

    Reuse illumination

--- src/app/pages/SearchResults/SearchResultsSkeleton.tsx
+++ src/app/pages/SearchResults/SearchResultsSkeleton.tsx
@@ -5,7 +5,7 @@ import { times } from 'lodash';
 export const SearchResultsSkeleton = () => (
   <div className={classes.root} aria-hidden>
     <div className={classes.inner}>
-      <div className={classes.gradient} />
+      <div className={classes.illumination} />
       {times(3, i => (
         <div className={classes.result} key={i}>
           <div className={classes.photo} />
