b213110942cb15799f62fb5e5d1ecc9f2814240e
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-03-20 21:39:42 +0200

    Get things to work

--- src/webpack/appGlobals.js
+++ src/webpack/appGlobals.js
@@ -7,8 +7,8 @@ module.exports.getAppGlobals = isNode => ({
   __FORCE_ENABLE_LOGGING__: isTest || Boolean(process.env.FORCE_ENABLE_LOGGING),
   __IS_SERVER__: isNode && !isTest,
   __IS_DEBUG__: isDebug,
-  __BRANCH__: process.env.BRANCH,
-  __COMMIT_ID__: process.env.COMMIT_ID,
+  __BRANCH__: isTest ? 'test' : process.env.BRANCH,
+  __COMMIT_ID__: isTest ? '123456' : process.env.COMMIT_ID,
   __BASE__: isProd
     ? 'https://hollowverse.com'
     : `http://localhost:${process.env.APP_SERVER_PORT || 3001}`,
