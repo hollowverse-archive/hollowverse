85c74c8a47ce3f35457b032cf04d984a6583cf2d
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-03-25 19:50:41 +0200

    Workaround ESLint TS parser issue

--- src/app/helpers/delay.ts
+++ src/app/helpers/delay.ts
@@ -1,7 +1,7 @@
-export const delay = async <T = undefined>(n = 3000, resolveValue?: T) => {
+export async function delay<T = undefined>(n = 3000, resolveValue?: T) {
   return new Promise<T>(resolve => {
     setTimeout(() => {
       resolve(resolveValue);
     }, n);
   });
-};
+}
