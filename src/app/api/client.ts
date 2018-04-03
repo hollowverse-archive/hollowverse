01976333cab6c139a139b5a767d807ea625fe4ad
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-04-03 18:06:40 +0300

    Use GET for API queries
    Eventually, when we implement user log-in, we may need to have two separate instances of `GraphQLClient`, one for read operations (which would use `GET` and can be cached) and one for mutations (which would use `POST` and not be cached).  We would need to set the `Authorization` header for both clients.

--- src/app/api/client.ts
+++ src/app/api/client.ts
@@ -1,4 +1,6 @@
 import { GraphQLClient } from 'graphql-request';
 import { getUniversalUrl } from 'helpers/getUniversalUrl';
 
-export const client = new GraphQLClient(getUniversalUrl(__API_ENDPOINT__));
+export const client = new GraphQLClient(getUniversalUrl(__API_ENDPOINT__), {
+  method: 'GET',
+});
