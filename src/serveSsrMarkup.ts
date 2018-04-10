311ab5d40afd2caeb7471070261edde4d5653287
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-04-10 21:21:08 +0300

    Define binary types

--- src/serveSsrMarkup.ts
+++ src/serveSsrMarkup.ts
@@ -2,7 +2,26 @@ import { APIGatewayProxyHandler } from 'aws-lambda'; // tslint:disable-line no-i
 import awsServerlessExpress from 'aws-serverless-express';
 import { mainServer } from './mainServer';
 
-const server = awsServerlessExpress.createServer(mainServer);
+const server = awsServerlessExpress.createServer(mainServer, null, [
+  'application/javascript',
+  'application/json',
+  'application/octet-stream',
+  'application/xml',
+  'font/eot',
+  'font/opentype',
+  'font/otf',
+  'image/x-icon',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml',
+  'text/comma-separated-values',
+  'text/css',
+  'text/html',
+  'text/javascript',
+  'text/plain',
+  'text/text',
+  'text/xml',
+]);
 
 export const serveSsrMarkup: APIGatewayProxyHandler = (event, context) => {
   awsServerlessExpress.proxy(server, event, context);
