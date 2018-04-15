6cc8334753188c3ead1a88ef84357a986e1b218f
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-04-16 00:10:54 +0300

    Fix clown check

--- clownOverrides/clownCallback.js
+++ clownOverrides/clownCallback.js
@@ -3,6 +3,15 @@ const { omit } = require('lodash');
 
 module.exports = function clownCallback(clownFs) {
   clownFs.editJson('package.json', json =>
-    omit(json, 'jest', 'devDependencies["npm-run-all"]'),
+    omit(
+      json,
+      'jest',
+      'devDependencies["npm-run-all"]',
+      'dependencies["@hollowverse/utils"]',
+    ),
   );
+
+  // We don't need this here as we use a custom Babel config based
+  // on target (server/client)
+  clownFs.remove('.babelrc');
 };
--- deploy.js
+++ deploy.js
@@ -4,17 +4,26 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 
 const shelljs = require('shelljs');
+const { decryptSecrets } = require('@hollowverse/utils/helpers/decryptSecrets');
 const {
   executeCommands,
 } = require('@hollowverse/utils/helpers/executeCommands');
 
-const { IS_PULL_REQUEST, BRANCH } = shelljs.env;
+const { ENC_PASS_SUMO, IS_PULL_REQUEST, BRANCH } = shelljs.env;
 
 const isPullRequest = IS_PULL_REQUEST !== 'false';
+
+const secrets = [
+  {
+    password: ENC_PASS_SUMO,
+    decryptedFilename: 'sumo.json',
+  },
+];
 
 async function main() {
   const buildCommands = ['yarn test', 'yarn coverage/report', 'yarn build'];
   const deploymentCommands = [
+    () => decryptSecrets(secrets, './secrets'),
     'cp yarn.lock package.json ./dist && cd dist && yarn --prod',
     `NODE_ENV=production serverless deploy --stage ${BRANCH} --aws-s3-accelerate`,
   ];
