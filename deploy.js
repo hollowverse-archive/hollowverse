e9d5f280d37c083c0377ca599c04429aecbf7db4
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-04-15 22:53:25 +0300

    Update deploy script

--- deploy.js
+++ deploy.js
@@ -4,37 +4,24 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 
 const shelljs = require('shelljs');
-const { decryptSecrets } = require('@hollowverse/utils/helpers/decryptSecrets');
 const {
   executeCommands,
 } = require('@hollowverse/utils/helpers/executeCommands');
 
-const { IS_PULL_REQUEST, BRANCH, ENC_PASS_SUMO } = shelljs.env;
+const { IS_PULL_REQUEST, BRANCH } = shelljs.env;
 
 const isPullRequest = IS_PULL_REQUEST !== 'false';
-
-const secrets = [
-  {
-    password: ENC_PASS_SUMO,
-    decryptedFilename: 'sumo.json',
-  },
-];
 
 async function main() {
   const buildCommands = ['yarn test', 'yarn coverage/report', 'yarn build'];
   const deploymentCommands = [
-    () => decryptSecrets(secrets, './secrets'),
     'cp yarn.lock package.json ./dist && cd dist && yarn --prod',
-    `NODE_ENV=production serverless deploy --stage production-${BRANCH} --aws-s3-accelerate`,
+    `NODE_ENV=production serverless deploy --stage ${BRANCH} --aws-s3-accelerate`,
   ];
 
   let isDeployment = false;
   if (isPullRequest === true) {
     console.info('Skipping deployment commands in PRs');
-  } else if (secrets.some(secret => secret.password === undefined)) {
-    console.info(
-      'Skipping deployment commands because some secrets are not provided',
-    );
   } else {
     isDeployment = true;
   }
