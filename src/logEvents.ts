b906b62f2cfb68b4d8d1acc116aba13a803fc5dc
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-04-18 17:40:57 +0300

    Remove unused file logEvents.ts

--- src/logEvents.ts
+++
@@ -1,5 +0,0 @@
-import { APIGatewayProxyHandler } from 'aws-lambda'; // tslint:disable-line no-implicit-dependencies
-
-export const logEvents: APIGatewayProxyHandler = async (_c, _d) => {
-  return 'ok';
-};
