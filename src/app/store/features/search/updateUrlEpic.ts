6f40ce88d0bf219a66351cfd5c5bd013555566a0
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-04-30 23:47:31 +0300

    Upgrade to RxJs 6 (#444)

--- src/app/store/features/search/updateUrlEpic.ts
+++ src/app/store/features/search/updateUrlEpic.ts
@@ -1,10 +1,10 @@
+import { map } from 'rxjs/operators';
 import { push, replace } from 'react-router-redux';
 
 import { Action, StoreState } from 'store/types';
 
 import { Epic } from 'redux-observable';
 
-import 'rxjs/add/operator/map';
 import { getRoutingState, isSearchPage } from 'store/features/url/selectors';
 import { isActionOfType } from 'store/helpers';
 import { LocationDescriptor } from 'history';
@@ -15,30 +15,32 @@ import { LocationDescriptor } from 'history';
  * reflect the search query.
  * @example `{ type: 'SEARCH_QUERY_CHANGED', query: 'Tom Ha' }` => '/search?query=Tom+Ha'
  */
-export const updateUrlEpic: Epic<Action, StoreState> = (action$, store) => {
-  return action$.ofType('SEARCH_QUERY_CHANGED', 'GO_TO_SEARCH').map(action => {
-    let descriptor: LocationDescriptor = {
-      pathname: '/search',
-    };
+export const updateUrlEpic: Epic<Action, StoreState> = (action$, state$) => {
+  return action$.ofType('SEARCH_QUERY_CHANGED', 'GO_TO_SEARCH').pipe(
+    map(action => {
+      let descriptor: LocationDescriptor = {
+        pathname: '/search',
+      };
 
-    const state = store.getState();
-    const location = getRoutingState(state).location;
+      const state = state$.value;
+      const location = getRoutingState(state).location;
 
-    if (isActionOfType(action, 'GO_TO_SEARCH') && isSearchPage(state)) {
-      descriptor = { ...location };
-    } else if (isActionOfType(action, 'SEARCH_QUERY_CHANGED')) {
-      const searchParams = new URLSearchParams();
-      const { query } = action.payload;
-      searchParams.append('query', query);
-      descriptor.search = searchParams.toString();
-    }
+      if (isActionOfType(action, 'GO_TO_SEARCH') && isSearchPage(state)) {
+        descriptor = { ...location };
+      } else if (isActionOfType(action, 'SEARCH_QUERY_CHANGED')) {
+        const searchParams = new URLSearchParams();
+        const { query } = action.payload;
+        searchParams.append('query', query);
+        descriptor.search = searchParams.toString();
+      }
 
-    return location && location.pathname === descriptor.pathname
-      ? // This is to avoid things like
-        // /search?query=t, /search?query=te, /search?query=tes, /search?query=test
-        // filling the browser history stack instead of the actual
-        // previous page
-        replace(descriptor)
-      : push(descriptor);
-  });
+      return location && location.pathname === descriptor.pathname
+        ? // This is to avoid things like
+          // /search?query=t, /search?query=te, /search?query=tes, /search?query=test
+          // filling the browser history stack instead of the actual
+          // previous page
+          replace(descriptor)
+        : push(descriptor);
+    }),
+  );
 };
