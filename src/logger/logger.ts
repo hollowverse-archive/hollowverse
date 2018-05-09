caeffd8bc3af664a36709d7841c625ca41b495a2
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-05-09 18:30:13 +0300

    Work around self-signed SSL certificate used in Splunk

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -6,6 +6,8 @@ import got from 'got';
 
 import { LoggedAction } from './types';
 import { isActionOfType } from '../app/store/helpers';
+import { Agent as HttpsAgent } from 'https';
+import { globalAgent as globalHttpAgent } from 'http';
 
 const { BRANCH, COMMIT_ID, SPLUNK_COLLECTOR_TOKEN } = process.env;
 
@@ -18,8 +20,11 @@ const transformActionForLogging = async (
   if (isActionOfType(action, 'UNHANDLED_ERROR_THROWN')) {
     const { message, source, line, column } = action.payload;
     if (source && line && column) {
-      const { body: sourceMap } = await got(`${source}.map?branch=${BRANCH}`, {
+      const { body: sourceMap } = await got(`${source}.map`, {
         json: true,
+        headers: {
+          Cookie: `branch=${BRANCH}`,
+        },
       });
       const consumer = new SourceMapConsumer(sourceMap);
       const originalPosition = consumer.originalPositionFor({ line, column });
@@ -42,13 +47,18 @@ export async function log(actions: LoggedAction[]) {
     .map(actions, transformActionForLogging)
     .map((action: LoggedAction) => ({
       event: action,
-      time: new Date(action.timestamp),
       host: 'Lambda',
       source: `hollowverse/${BRANCH}/${COMMIT_ID}`,
     }));
 
   await got.post(COLLECTOR_URL, {
     body: transformedActions.map(event => JSON.stringify(event)).join(''),
+    agent: {
+      http: globalHttpAgent,
+      https: new HttpsAgent({
+        rejectUnauthorized: false,
+      }),
+    },
     headers: {
       Authorization: `Splunk ${SPLUNK_COLLECTOR_TOKEN}`,
     },
