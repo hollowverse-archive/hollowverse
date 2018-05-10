f8e29dd59e8a2a2ac3624c3cbb639050fa00636f
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-05-11 02:35:18 +0300

    Switch from SumoLogic to Splunk (#457)

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,20 +1,17 @@
 // tslint:disable no-console
 
 import bluebird from 'bluebird';
-import { SourceMapConsumer } from 'source-map';
 import got from 'got';
-import { URL } from 'url';
-
-import { LoggedAction } from './types';
+import { globalAgent as globalHttpAgent } from 'http';
+import { Agent as HttpsAgent } from 'https';
+import { SourceMapConsumer } from 'source-map';
 import { isActionOfType } from '../app/store/helpers';
+import { LoggedAction } from './types';
 
-const { BRANCH, COMMIT_ID, SUMO_COLLECTOR_ID } = process.env;
+const { BRANCH, COMMIT_ID, SPLUNK_COLLECTOR_TOKEN } = process.env;
 
-const RECEIVER_URL =
-  'https://endpoint2.collection.us2.sumologic.com/receiver/v1/http/';
-
-// tslint:disable-next-line no-non-null-assertion
-const COLLECTOR_URL = new URL(SUMO_COLLECTOR_ID!, RECEIVER_URL).toString();
+const COLLECTOR_URL =
+  'https://input-prd-p-kwnk36xd58jf.cloud.splunk.com:8088/services/collector/event';
 
 const transformActionForLogging = async (
   action: LoggedAction,
@@ -22,8 +19,11 @@ const transformActionForLogging = async (
   if (isActionOfType(action, 'UNHANDLED_ERROR_THROWN')) {
     const { message, source, line, column } = action.payload;
     if (source && line && column) {
-      const { body: sourceMap } = await got(`${source}.map?branch=${BRANCH}`, {
+      const { body: sourceMap } = await got(`${source}.map`, {
         json: true,
+        headers: {
+          Cookie: `branch=${BRANCH}`,
+        },
       });
       const consumer = new SourceMapConsumer(sourceMap);
       const originalPosition = consumer.originalPositionFor({ line, column });
@@ -45,16 +45,21 @@ export async function log(actions: LoggedAction[]) {
   const transformedActions = await bluebird
     .map(actions, transformActionForLogging)
     .map((action: LoggedAction) => ({
-      ...action,
-      timestamp: new Date(action.timestamp),
+      event: action,
+      host: 'HollowverseWebsite',
+      source: `${BRANCH}/${COMMIT_ID}`,
     }));
 
   await got.post(COLLECTOR_URL, {
-    json: true,
-    body: transformedActions,
+    body: transformedActions.map(event => JSON.stringify(event)).join(''),
+    agent: {
+      http: globalHttpAgent,
+      https: new HttpsAgent({
+        rejectUnauthorized: false,
+      }),
+    },
     headers: {
-      'X-Sumo-Category': `${BRANCH}/${COMMIT_ID}`,
-      'X-Sumo-Host': 'Lambda',
+      Authorization: `Splunk ${SPLUNK_COLLECTOR_TOKEN}`,
     },
   });
 }
