2d0c3555db2fd3ce3a180d94c93ccc26f47d833e
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-06-10 10:01:46 +0300

    Check whether browser is supported in log events (#521)

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -12,13 +12,17 @@ import memoizePromise from 'p-memoize';
 import { UAParser } from 'ua-parser-js';
 import { memoize } from 'lodash';
 import { LogBatch } from 'store/types';
+import { matchesUA } from 'browserslist-useragent';
 
 const parser = new UAParser();
 
 const parseUserAgent = memoize((userAgent: string) => {
   parser.setUA(userAgent);
 
-  return parser.getResult();
+  return {
+    ...parser.getResult(),
+    isSupported: matchesUA(userAgent, { _allowHigherVersions: true }),
+  };
 });
 
 const { BRANCH, COMMIT_ID } = process.env;
---
+++ src/typings/browserslist-useragent.d.ts
@@ -0,0 +1,42 @@
+declare module 'browserslist-useragent' {
+  type Options = {
+    /**
+     * Manually provide a browserslist query (or an array of queries).
+     * Specifying this overrides the browserslist configuration specified
+     * in your project.
+     */
+    browsers?: string[];
+
+    /**
+     * When multiple browserslist enviroments are specified,
+     * pick the config belonging to this environment.
+     */
+    env?: string;
+
+    /**
+     * Ignore differences in patch browser numbers
+     *
+     * @default true
+     */
+    ignorePatch?: boolean;
+
+    /**
+     * Ignore differences in minor browser versions
+     *
+     * @default false
+     */
+    ignoreMinor?: boolean;
+
+    /**
+     * In case you're unable to keep this package up-to-date, you can set the
+     * `_allowHigherVersions` to `true`.
+     * For all the browsers specified in your browserslist query,
+     * this will return a match if the user agent version is equal to or
+     * higher than those specified in your browserslist query. Use this
+     * with care though, since it's a wildcard, and only lightly tested.
+     */
+    _allowHigherVersions?: boolean;
+  };
+
+  export function matchesUA(userAgent: string, options: Options): boolean;
+}
