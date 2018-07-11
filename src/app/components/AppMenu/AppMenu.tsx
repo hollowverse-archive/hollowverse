7ff4da2b0b85e798172f2c03be095bcd3e97d3b1
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-07-11 18:38:40 +0300

    Revert "Use Switch component from material-ui"
    This reverts commit 9d851c0b9e71970f56c275f702e2947c1955c142.

--- src/app/components/AppMenu/AppMenu.tsx
+++ src/app/components/AppMenu/AppMenu.tsx
@@ -7,13 +7,13 @@ import {
   MenuItemWithLink,
   MenuItemWithButton,
   MenuItemWithChild,
+  MenuItemWithToggle,
 } from './MenuItem';
 
 import { PersonPhoto } from 'components/PersonPhoto/PersonPhoto';
 import { SvgIcon } from 'components/SvgIcon/SvgIcon';
 import { Paper } from 'components/Paper/Paper';
 import closeIcon from 'icons/close.svg';
-import Switch from '@material-ui/core/Switch';
 
 import classes from './AppMenu.module.scss';
 
@@ -119,16 +119,13 @@ export class AppMenu extends React.PureComponent<Props> {
                 {Separator}
                 {isNightModeAvailable && (
                   <>
-                    <MenuItemWithChild
-                      factory="label"
-                      className={classes.switch}
+                    <MenuItemWithToggle
+                      defaultChecked={isNightModeEnabled}
+                      onChange={this.toggleNightMode}
                     >
-                      <div>Night mode</div>
-                      <Switch
-                        checked={isNightModeEnabled}
-                        onChange={this.toggleNightMode}
-                      />
-                    </MenuItemWithChild>
+                      Night mode
+                    </MenuItemWithToggle>
+
                     {Separator}
                   </>
                 )}
