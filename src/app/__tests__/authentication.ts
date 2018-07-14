52163e285987d685001d95e9ea1cc0819afdcea0
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-07-15 01:38:52 +0300

    Add more auth tests

--- src/app/__tests__/authentication.ts
+++ src/app/__tests__/authentication.ts
@@ -2,6 +2,7 @@ import {
   createClientSideTestContext,
   ClientSideTestContext,
 } from 'helpers/testHelpers';
+import { getFbSdkAuthState } from 'store/features/auth/reducer';
 
 describe('authentication', () => {
   let context: ClientSideTestContext;
@@ -21,5 +22,34 @@ describe('authentication', () => {
       'auth.authResponseChange',
       expect.any(Function),
     );
+  });
+
+  it('shows login button', async () => {
+    expect(
+      context
+        .openAppMenu()
+        .find('button')
+        .filterWhere(el => !!el.text().match(/log in/i)),
+    ).toBeDefined();
+  });
+});
+
+describe('on FB SDK initialization failure', () => {
+  let context: ClientSideTestContext;
+
+  beforeEach(async () => {
+    context = await createClientSideTestContext({
+      epicDependenciesOverrides: {
+        async getFbSdk() {
+          throw new Error('Script failed to load');
+        },
+      },
+    });
+  });
+
+  it('error state is handled', () => {
+    expect(getFbSdkAuthState(context.store.getState())).toMatchObject({
+      state: 'error',
+    });
   });
 });
