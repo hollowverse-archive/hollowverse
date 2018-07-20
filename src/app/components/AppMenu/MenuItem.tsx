f7859bb6195348e7af80f26c24cc720d28c94953
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-07-20 21:47:20 +0300

    Remove obsolete comment

--- src/app/components/AppMenu/MenuItem.tsx
+++ src/app/components/AppMenu/MenuItem.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import MenuItem, { MenuItemProps as Props } from '@material-ui/core/MenuItem';
 import { Link, LinkProps } from 'react-router-dom';
 
-// Strongly-typed convenience wrappers for `MenuItemWithChild`
 export const MenuItemWithLink = (props: Props & LinkProps) => (
   <MenuItem component={Link as any} {...props} />
 );
