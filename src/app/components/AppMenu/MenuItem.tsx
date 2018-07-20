146aca2ddf716d58f9ebb0dc69d000c7c234d105
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-07-20 19:09:00 +0300

    Remove MenuItemWithButton

--- src/app/components/AppMenu/MenuItem.tsx
+++ src/app/components/AppMenu/MenuItem.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import MenuItem, { MenuItemProps as Props } from '@material-ui/core/MenuItem';
 import { Link, LinkProps } from 'react-router-dom';
-import { Omit } from 'typings/typeHelpers';
 
 // tslint:disable-next-line function-name
 export function MenuItemWithChild<FactoryProps extends { className?: string }>(
@@ -16,7 +15,3 @@ export function MenuItemWithChild<FactoryProps extends { className?: string }>(
 export const MenuItemWithLink = (props: Props & LinkProps) => (
   <MenuItem component={Link as any} {...props} />
 );
-
-export const MenuItemWithButton = (
-  props: Props & Omit<React.InputHTMLAttributes<HTMLButtonElement>, 'children'>,
-) => <MenuItem component="button" {...props} />;
