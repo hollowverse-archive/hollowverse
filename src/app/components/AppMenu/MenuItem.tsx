c9d15d39db1a9c0dc4a6cea826660dd6281593e2
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-07-20 21:35:04 +0300

    Remove unused component

--- src/app/components/AppMenu/MenuItem.tsx
+++ src/app/components/AppMenu/MenuItem.tsx
@@ -2,15 +2,6 @@ import React from 'react';
 import MenuItem, { MenuItemProps as Props } from '@material-ui/core/MenuItem';
 import { Link, LinkProps } from 'react-router-dom';
 
-// tslint:disable-next-line function-name
-export function MenuItemWithChild<FactoryProps extends { className?: string }>(
-  props: Props & FactoryProps & { factory: string | React.ComponentType<any> },
-) {
-  const { factory: Factory } = props;
-
-  return <MenuItem component={Factory} {...props} />;
-}
-
 // Strongly-typed convenience wrappers for `MenuItemWithChild`
 export const MenuItemWithLink = (props: Props & LinkProps) => (
   <MenuItem component={Link as any} {...props} />
