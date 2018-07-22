0f993727053e2c8315563902fbfd3e3816f4eb47
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-07-22 20:16:25 +0300

    Enable no-restricted-imports rule for ESLint

--- src/app/components/LocationAwareMenu/LocationAwareMenu.tsx
+++ src/app/components/LocationAwareMenu/LocationAwareMenu.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+/* eslint-disable-next-line no-restricted-imports */
 import Menu, { MenuProps } from '@material-ui/core/Menu';
 import { RouteComponentProps, withRouter } from 'react-router';
 
