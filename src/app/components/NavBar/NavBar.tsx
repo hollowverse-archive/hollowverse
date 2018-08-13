55e32b1aa38f24e49221b545bbb56e138434cc9c
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-08-13 16:57:33 +0300

    Remove SCSS styles from NavBar

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -3,8 +3,6 @@ import cc from 'classcat';
 
 import { RouteComponentProps, Route, Switch } from 'react-router';
 
-import classes from './NavBar.module.scss';
-
 import { Sticky } from 'components/Sticky/Sticky';
 
 import SearchIcon from '@material-ui/icons/Search';
@@ -12,13 +10,50 @@ import IconButton from '@material-ui/core/IconButton';
 import textLogo from '!!file-loader!assets/textLogo.svg';
 
 import { ConnectedSearchBar } from 'components/NavBar/ConnectedSearchBar';
-// import { ConnectedAppMenu } from 'components/AppMenu/ConnectedAppMenu';
+import { ConnectedAppMenu } from 'components/AppMenu/ConnectedAppMenu';
+
 import { Link } from 'react-router-dom';
+
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Tooltip from '@material-ui/core/Tooltip';
 import LinearProgress from '@material-ui/core/LinearProgress';
-import { ConnectedAppMenu } from 'components/AppMenu/ConnectedAppMenu';
+import createStyles from '@material-ui/core/styles/createStyles';
+import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
+import { Theme } from '@material-ui/core';
+
+const styles = (theme: Theme) =>
+  createStyles({
+    isHidden: {
+      visibility: 'hidden',
+    },
+    maxWidth: {
+      width: '100%',
+      maxWidth: theme.breakpoints.values.md,
+    },
+    logoWrapper: {
+      animation: 'navbar-switch 0.15s',
+      display: 'flex',
+      flexGrow: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    logo: {
+      display: 'flex',
+      textTransform: 'uppercase',
+      letterSpacing: 1,
+    },
+    '@keyframes navbar-switch': {
+      from: {
+        opacity: 0,
+        transform: 'translateY(10px)',
+      },
+      to: {
+        opacity: 1,
+        transform: 'translateY(0)',
+      },
+    },
+  });
 
 export type OwnProps = {
   title: string;
@@ -32,76 +67,82 @@ export type StateProps = {
 
 export type DispatchProps = {};
 
-type Props = OwnProps & StateProps & DispatchProps;
+type Props = OwnProps &
+  StateProps &
+  DispatchProps &
+  WithStyles<ReturnType<typeof styles>>;
 
-export const NavBar = class extends React.Component<
-  Props & RouteComponentProps<any>
-> {
-  render() {
-    const {
-      title,
-      shouldFocusSearch,
-      shouldShowGlobalProgress,
-      isHomePage,
-    } = this.props;
+export const NavBar = withStyles(styles)(
+  class extends React.Component<Props & RouteComponentProps<any>> {
+    render() {
+      const {
+        title,
+        shouldFocusSearch,
+        shouldShowGlobalProgress,
+        isHomePage,
+        classes,
+      } = this.props;
 
-    return (
-      <Sticky rootMargin="30% 0% 0% 0%" height={48}>
-        {isSticking => (
-          <>
-            <AppBar
-              position="static"
-              color="inherit"
-              style={{
-                alignItems: 'center',
-              }}
-            >
-              <Toolbar variant="dense" className={classes.maxWidth}>
-                <ConnectedAppMenu />
-                <Switch>
-                  <Route path="/search">
-                    <ConnectedSearchBar />
-                  </Route>
-                  <Route>
-                    {isSticking || shouldFocusSearch ? (
-                      <ConnectedSearchBar />
-                    ) : (
-                      <>
-                        <div className={classes.logoWrapper}>
-                          <Link
-                            title="Homepage"
-                            className={classes.logo}
-                            to="/"
-                          >
-                            <img src={textLogo} alt={title} />
-                          </Link>
-                        </div>
-                        <Tooltip title="Search">
-                          <IconButton
-                            aria-label="Search"
-                            className={cc([{ [classes.isHidden]: isHomePage }])}
-                            component={Link as any}
-                            {...{ to: '/search' } as any}
-                          >
-                            <SearchIcon />
-                          </IconButton>
-                        </Tooltip>
-                      </>
-                    )}
-                  </Route>
-                </Switch>
-              </Toolbar>
-              <LinearProgress
+      return (
+        <Sticky rootMargin="30% 0% 0% 0%" height={48}>
+          {isSticking => (
+            <>
+              <AppBar
+                position="static"
+                color="inherit"
                 style={{
-                  visibility: shouldShowGlobalProgress ? undefined : 'hidden',
-                  width: '100%',
-                  height: '1px',
+                  alignItems: 'center',
                 }}
-              />
-            </AppBar>
-          </>
-        )}
-      </Sticky>
-    );
-  }
-};
+              >
+                <Toolbar variant="dense" className={classes.maxWidth}>
+                  <ConnectedAppMenu />
+                  <Switch>
+                    <Route path="/search">
+                      <ConnectedSearchBar />
+                    </Route>
+                    <Route>
+                      {isSticking || shouldFocusSearch ? (
+                        <ConnectedSearchBar />
+                      ) : (
+                        <>
+                          <div className={classes.logoWrapper}>
+                            <Link
+                              title="Homepage"
+                              className={classes.logo}
+                              to="/"
+                            >
+                              <img src={textLogo} alt={title} />
+                            </Link>
+                          </div>
+                          <Tooltip title="Search">
+                            <IconButton
+                              aria-label="Search"
+                              className={cc([
+                                { [classes.isHidden]: isHomePage },
+                              ])}
+                              component={Link as any}
+                              {...{ to: '/search' } as any}
+                            >
+                              <SearchIcon />
+                            </IconButton>
+                          </Tooltip>
+                        </>
+                      )}
+                    </Route>
+                  </Switch>
+                </Toolbar>
+                <LinearProgress
+                  style={{
+                    visibility: shouldShowGlobalProgress ? undefined : 'hidden',
+                    width: '100%',
+                    height: '1px',
+                  }}
+                />
+              </AppBar>
+            </>
+          )}
+        </Sticky>
+      );
+    }
+  },
+);
