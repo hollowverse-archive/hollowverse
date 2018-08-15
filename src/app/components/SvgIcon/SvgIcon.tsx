3e479aeb93fe48f5e1c40fe47a10b7a3b5216a4f
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-08-15 17:28:10 +0300

    Migrate SvgIcon

--- src/app/components/SvgIcon/SvgIcon.tsx
+++ src/app/components/SvgIcon/SvgIcon.tsx
@@ -1,33 +1,47 @@
 import React from 'react';
 import cc from 'classcat';
+import {
+  withStyles,
+  createStyles,
+  WithStyles,
+  Theme,
+} from '@material-ui/core/styles';
 
-import classes from './SvgIcon.module.scss';
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      '--default-fill': theme.palette.text.secondary,
+    },
+  });
 
 type Props = React.HTMLAttributes<SVGElement> &
   SpriteSymbol & {
     /** Height of icon in pixels */
     size?: number | string;
     color?: string;
-  };
+  } & WithStyles<ReturnType<typeof styles>>;
 
-export const SvgIcon = ({
-  id: _, // Remove `id` from `rest` so it does not get assigned to this SVG element
-  toString: __,
-  url,
-  viewBox,
-  className,
-  color,
-  size = 32,
-  ...rest
-}: Props) => (
-  <svg
-    {...rest}
-    height={size}
-    role="presentation"
-    viewBox={viewBox}
-    className={cc([className, classes.root])}
-    style={{ '--fill': color } as any}
-  >
-    <use xlinkHref={url} />
-  </svg>
+export const SvgIcon = withStyles(styles)(
+  ({
+    id: _, // Remove `id` from `rest` so it does not get assigned to this SVG element
+    toString: __,
+    url,
+    viewBox,
+    className,
+    classes,
+    color,
+    size = 32,
+    ...rest
+  }: Props) => (
+    <svg
+      {...rest}
+      height={size}
+      role="presentation"
+      viewBox={viewBox}
+      className={cc([className, classes.root])}
+      style={{ '--fill': color } as any}
+    >
+      <use xlinkHref={url} />
+    </svg>
+  ),
 );
