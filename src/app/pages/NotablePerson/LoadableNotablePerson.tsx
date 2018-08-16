2d5f441e52e4ec0c94658d3f18702947ff4a9f8e
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-08-16 15:11:24 +0300

    Migrate NotablePerson and related components

--- src/app/pages/NotablePerson/LoadableNotablePerson.tsx
+++ src/app/pages/NotablePerson/LoadableNotablePerson.tsx
@@ -3,15 +3,38 @@ import loadable from 'react-loadable';
 import { loadableDefaultOptions } from 'helpers/loadableDefaultOptions';
 import { NotablePersonBody } from './NotablePersonBody';
 
-import classes from './LoadableNotablePerson.module.scss';
+import {
+  withStyles,
+  createStyles,
+  WithStyles,
+  Theme,
+} from '@material-ui/core/styles';
+
+export const styles = ({ breakpoints }: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      paddingBottom: 20,
+      '& > *': {
+        width: '100%',
+        maxWidth: breakpoints.values.sm,
+      },
+    },
+  });
+
+type Props = WithStyles<ReturnType<typeof styles>>;
+
+const LoadingComponent = withStyles(styles)<Props>(({ classes }) => (
+  <div className={classes.root}>
+    <NotablePersonBody />
+  </div>
+));
 
 export const LoadableNotablePerson = loadable({
   ...loadableDefaultOptions,
   loader: async () =>
     import('./NotablePerson').then(module => module.NotablePerson),
-  loading: () => (
-    <div className={classes.root}>
-      <NotablePersonBody />
-    </div>
-  ),
+  loading: LoadingComponent as any,
 });
