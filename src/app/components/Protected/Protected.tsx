fed0d150d6c2acf7159663fd36811a4af30952ac
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-08-25 21:50:30 +0300

    Document code

--- src/app/components/Protected/Protected.tsx
+++ src/app/components/Protected/Protected.tsx
@@ -10,12 +10,30 @@ type StateProps = {
 };
 
 export type OwnProps = {
+  /**
+   * Users who have any of these roles will be allowed
+   */
   authorizedRoles?: UserRole[];
+  /**
+   * Unless `children` is a function, its value will be rendered if
+   * the user is authenticated and has any of the required roles passed
+   * via `authorizedRoles`. Otherwise, the component will render nothing.
+   *
+   * If `children` is function, it will be passed an object
+   * which contains information about whether the user is authorized and
+   * it is up to that function to decide what to render or if
+   * it will render anything at all.
+   */
   children: React.ReactNode | ((result: AuthorizationState) => React.ReactNode);
 };
 
 type Props = OwnProps & StateProps;
 
+/**
+ * A component that conditionally renders its `children` based on
+ * whether the current viewer is logged in and if it has sufficient
+ * permissions to view the content.
+ */
 export const Protected = connect(
   (state: StoreState, { authorizedRoles }: OwnProps) => ({
     authorizationState: getAuthorizationState(state)(authorizedRoles),
