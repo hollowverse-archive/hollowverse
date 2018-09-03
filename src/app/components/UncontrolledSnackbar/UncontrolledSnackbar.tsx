7fc6be1802c8df6564d31543480f7c1b2e91fc8e
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-09-03 22:41:05 +0300

    Extract UncontrolledSnackbar

---
+++ src/app/components/UncontrolledSnackbar/UncontrolledSnackbar.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Snackbar, { SnackbarProps } from '@material-ui/core/Snackbar';
+import { Omit } from 'typings/typeHelpers';
+
+type UncontrolledSnackbarChildrenProps = {
+  close(): void;
+};
+
+type Props = Omit<SnackbarProps, 'action'> & {
+  renderAction(
+    props: UncontrolledSnackbarChildrenProps,
+  ): SnackbarProps['action'];
+};
+
+type State = {
+  isOpen: boolean;
+};
+
+export const UncontrolledSnackbar = class extends React.Component<
+  Props,
+  State
+> {
+  state: State = {
+    isOpen: this.props.open,
+  };
+
+  handleClose = () => {
+    this.setState({ isOpen: false });
+  };
+
+  render() {
+    const { renderAction, ...rest } = this.props;
+
+    return (
+      <Snackbar
+        {...rest}
+        open={this.state.isOpen}
+        onClose={this.handleClose}
+        action={renderAction({ close: this.handleClose })}
+      />
+    );
+  }
+};
