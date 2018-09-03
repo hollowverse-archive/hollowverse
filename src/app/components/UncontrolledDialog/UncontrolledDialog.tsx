7d40acd4239c460ca08c42d33aabc5e71c23b8ca
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-09-03 21:04:33 +0300

    Extract uncontrolled dialog

---
+++ src/app/components/UncontrolledDialog/UncontrolledDialog.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Dialog, { DialogProps } from '@material-ui/core/Dialog';
+import { Omit } from 'typings/typeHelpers';
+
+type UncontrolledDialogChildrenProps = {
+  close(): void;
+};
+
+type Props = Omit<DialogProps, 'children'> & {
+  children(props: UncontrolledDialogChildrenProps): React.ReactNode;
+};
+
+type State = {
+  isOpen: boolean;
+};
+
+export const UncontrolledDialog = class extends React.Component<Props, State> {
+  state: State = {
+    isOpen: this.props.open,
+  };
+
+  handleClose = () => {
+    this.setState({ isOpen: false });
+  };
+
+  render() {
+    const { children, ...rest } = this.props;
+
+    return (
+      <Dialog {...rest} open={this.state.isOpen}>
+        {children({ close: this.handleClose })}
+      </Dialog>
+    );
+  }
+};
--- src/app/pages/Moderation/Users/Users.tsx
+++ src/app/pages/Moderation/Users/Users.tsx
@@ -13,11 +13,16 @@ import MenuItem from '@material-ui/core/MenuItem';
 import IconButton from '@material-ui/core/IconButton';
 import Avatar from '@material-ui/core/Avatar';
 import Tab from '@material-ui/core/Tab';
-import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogActions from '@material-ui/core/DialogActions';
+import Button from '@material-ui/core/Button';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import MoreIcon from '@material-ui/icons/MoreVert';
+import BlockIcon from '@material-ui/icons/Block';
+// import UnblockIcon from '@material-ui/icons/Unblock';
 import { withStyles, createStyles, Theme } from '@material-ui/core/styles';
 
 import {
@@ -32,12 +37,14 @@ import changeUserBanStatusMutation from '!!graphql-tag/loader!./ChangeUserBanSta
 
 import { createPulseAnimation } from 'helpers/animations';
 import { callAll } from 'helpers/callAll';
+import { ArrayElement } from 'typings/typeHelpers';
 import { MessageWithIcon } from 'components/MessageWithIcon/MessageWithIcon';
 import { LocationAwareTabs } from 'components/LocationAwareTabs/LocationAwareTabs';
 import {
   UncontrolledMenu,
   UncontrolledMenuButtonProps,
 } from 'components/UncontrolledMenu/UncontrolledMenu';
+import { UncontrolledDialog } from 'components/UncontrolledDialog/UncontrolledDialog';
 
 const LoadingListPlaceholder = withStyles((theme: Theme) => {
   const pulse = createPulseAnimation(theme);
@@ -78,6 +85,83 @@ const renderUserMenuItemButton: (
   </IconButton>
 );
 
+const renderUserMenuItem = (variables: UsersQueryVariables) => ({
+  node: { id, photoUrl, name, email, isBanned },
+}: ArrayElement<UsersQuery['users']['edges']>) => (
+  <ListItem key={id}>
+    <Avatar src={photoUrl || undefined} />
+    <ListItemText primary={name} secondary={email} />
+    <Mutation<ChangeUserBanStatusMutation, ChangeUserBanStatusMutationVariables>
+      mutation={changeUserBanStatusMutation}
+      variables={{ newValue: !isBanned, userId: id }}
+      refetchQueries={[{ query: usersQuery, variables }]}
+    >
+      {(changeUserBanStatus, { loading, called, error, data }) => (
+        <>
+          <ListItemSecondaryAction>
+            <UncontrolledMenu
+              renderButton={renderUserMenuItemButton}
+              anchorOrigin={{
+                horizontal: 'right',
+                vertical: 'center',
+              }}
+              id={`user-action-menu-${id}`}
+            >
+              {props => {
+                console.log({ loading, called, error, data });
+
+                return (
+                  <MenuItem
+                    {...props}
+                    onClick={callAll(props.onClick, () => {
+                      changeUserBanStatus();
+                    })}
+                    disabled={loading}
+                  >
+                    {isBanned ? null : (
+                      <ListItemIcon>
+                        {loading ? (
+                          <CircularProgress size={24} />
+                        ) : (
+                          <BlockIcon />
+                        )}
+                      </ListItemIcon>
+                    )}
+                    {isBanned ? 'Unban User' : 'Ban User'}
+                  </MenuItem>
+                );
+              }}
+            </UncontrolledMenu>
+          </ListItemSecondaryAction>
+          {data && data.changeUserBanStatus.result.state === 'error' ? (
+            <UncontrolledDialog open>
+              {({ close }) => (
+                <>
+                  <DialogTitle>Failed to change ban status of user</DialogTitle>
+                  <DialogContent>
+                    <DialogContentText>
+                      {(data.changeUserBanStatus.result as any).errors.map(
+                        ({ message }: any) => (
+                          <span key={message}>{message}</span>
+                        ),
+                      )}
+                    </DialogContentText>
+                  </DialogContent>
+                  <DialogActions>
+                    <Button onClick={close}>Dismiss</Button>
+                  </DialogActions>
+                </>
+              )}
+            </UncontrolledDialog>
+          ) : (
+            false
+          )}
+        </>
+      )}
+    </Mutation>
+  </ListItem>
+);
+
 const renderUserList = ({
   data,
   fetchMore,
@@ -101,69 +185,7 @@ const renderUserList = ({
 
   return (
     <List>
-      {edges.map(({ node: { id, photoUrl, name, email, isBanned } }) => (
-        <ListItem key={id}>
-          <Avatar src={photoUrl || undefined} />
-          <ListItemText primary={name} secondary={email} />
-          <Mutation<
-            ChangeUserBanStatusMutation,
-            ChangeUserBanStatusMutationVariables
-          >
-            mutation={changeUserBanStatusMutation}
-            variables={{ newValue: !isBanned, userId: id }}
-            refetchQueries={[{ query: usersQuery, variables }]}
-          >
-            {(changeUserBanStatus, { loading, called, error, data }) => (
-              <>
-                <ListItemSecondaryAction>
-                  <UncontrolledMenu
-                    renderButton={renderUserMenuItemButton}
-                    anchorOrigin={{
-                      horizontal: 'right',
-                      vertical: 'center',
-                    }}
-                    id={`user-action-menu-${id}`}
-                  >
-                    {props => {
-                      console.log({ loading, called, error, data });
-
-                      return (
-                        <MenuItem
-                          {...props}
-                          onClick={callAll(props.onClick, () => {
-                            changeUserBanStatus();
-                          })}
-                          disabled={called}
-                        >
-                          {isBanned ? 'Unban User' : 'Ban User'}
-                        </MenuItem>
-                      );
-                    }}
-                  </UncontrolledMenu>
-                </ListItemSecondaryAction>
-                {data && data.changeUserBanStatus.result.state === 'error' ? (
-                  <Dialog open>
-                    <DialogTitle>
-                      Failed to change ban status of user
-                    </DialogTitle>
-                    <DialogContent>
-                      <DialogContentText>
-                        {(data.changeUserBanStatus.result as any).errors.map(
-                          ({ message }: any) => (
-                            <div key={message}>{message}</div>
-                          ),
-                        )}
-                      </DialogContentText>
-                    </DialogContent>
-                  </Dialog>
-                ) : (
-                  false
-                )}
-              </>
-            )}
-          </Mutation>
-        </ListItem>
-      ))}
+      {edges.map(renderUserMenuItem(variables))}
       {hasNextPage ? (
         <>
           <IntersectionObserver
