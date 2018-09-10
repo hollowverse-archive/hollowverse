8e7178136ab3852e3438a1f98a6075479ceef891
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-09-10 20:01:04 +0300

    Use Card component to display quotes, get notablePerson name

--- src/app/pages/Moderation/Quotes/Quotes.tsx
+++ src/app/pages/Moderation/Quotes/Quotes.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+
 import { Query } from 'react-apollo';
 import {
   NotablePersonEventsQuery,
@@ -29,14 +35,31 @@ export const Quotes = () => (
             notablePeopleEvents: { edges },
           } = data;
 
-          return edges.map(({ node: { id, quote, submittedBy: { name } } }) => {
-            return (
-              <div>
-                <div key={id}>{quote}</div>
-                Submitted by: {name}
-              </div>
-            );
-          });
+          return edges.map(
+            ({ node: { id, quote, submittedBy, notablePerson } }) => {
+              return (
+                <Card key={id}>
+                  <CardContent>
+                    <Typography component="blockquote" variant="subheading">
+                      {notablePerson.name}
+                    </Typography>
+                    <Typography component="blockquote" variant="headline">
+                      {quote}
+                    </Typography>
+                    Submitted by: {submittedBy.name}
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      Allow
+                    </Button>
+                    <Button size="small" color="primary">
+                      Remove
+                    </Button>
+                  </CardActions>
+                </Card>
+              );
+            },
+          );
         }
 
         return null;
