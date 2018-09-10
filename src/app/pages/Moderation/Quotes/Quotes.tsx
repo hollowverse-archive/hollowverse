c77afddd72b0aa1424a2b8706b9f4f5e0ca84fc3
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-09-10 22:30:13 +0300

    Add and use mutation to change review status

--- src/app/pages/Moderation/Quotes/Quotes.tsx
+++ src/app/pages/Moderation/Quotes/Quotes.tsx
@@ -6,19 +6,22 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Chip from '@material-ui/core/Chip';
-import RemovedIcon from '@material-ui/icons/BlockOutlined';
-import AllowedIcon from '@material-ui/icons/CheckOutlined';
-import NotReviewedIcon from '@material-ui/icons/WatchLaterOutlined';
+import RemovedIcon from '@material-ui/icons/BlockRounded';
+import AllowedIcon from '@material-ui/icons/CheckRounded';
+import NotReviewedIcon from '@material-ui/icons/WatchLaterRounded';
 
 import formatDate from 'date-fns/format';
 
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import {
   NotablePersonEventsQuery,
   NotablePersonEventsQueryVariables,
   NotablePersonEventReviewStatus,
+  ChangeNotablePersonEventReviewStatusMutation,
+  ChangeNotablePersonEventReviewStatusMutationVariables,
 } from 'api/types';
 import notablePersonEventsQuery from '!!graphql-tag/loader!./NotablePersonEventsQuery.graphql';
+import changeNotablePresonEventReviewStatusMutation from '!!graphql-tag/loader!./ChangeNotablePersonEventReviewStatusMutation.graphql';
 
 import { prettifyUrl } from 'helpers/prettifyUrl';
 
@@ -37,6 +40,7 @@ const iconByReviewStatus: Record<
   NOT_REVIEWED: <NotReviewedIcon />,
 };
 
+// tslint:disable max-func-body-length
 export const Quotes = () => (
   <>
     <Typography align="center" variant="title" component="h1">
@@ -74,42 +78,79 @@ export const Quotes = () => (
               const date = happenedOn ? new Date(happenedOn) : undefined;
 
               return (
-                <Card key={id}>
-                  <CardContent>
-                    <Typography component="blockquote" variant="subheading">
-                      {notablePerson.name}
-                    </Typography>
-                    <Typography component="blockquote" variant="headline">
-                      {quote}
-                    </Typography>
-                    <div>Submitted by: {submittedBy.name}</div>
-                    <div>Source: {prettifyUrl(sourceUrl)}</div>
-                    <div>
-                      {date !== undefined ? (
-                        <time dateTime={date.toISOString()}>
-                          {formatDate(date, 'MMMM D, YYYY')}
-                        </time>
-                      ) : null}
-                    </div>
-                  </CardContent>
-                  <CardActions>
-                    <Chip
-                      avatar={iconByReviewStatus[reviewStatus]}
-                      label={labelByReviewStatus[reviewStatus]}
-                      variant="outlined"
-                    />
-                    {reviewStatus !== 'ALLOWED' ? (
-                      <Button size="small" color="primary">
-                        Allow
-                      </Button>
-                    ) : null}
-                    {reviewStatus !== 'REMOVED' ? (
-                      <Button size="small" color="primary">
-                        Remove
-                      </Button>
-                    ) : null}
-                  </CardActions>
-                </Card>
+                <Mutation<
+                  ChangeNotablePersonEventReviewStatusMutation,
+                  ChangeNotablePersonEventReviewStatusMutationVariables
+                >
+                  mutation={changeNotablePresonEventReviewStatusMutation}
+                  refetchQueries={[{ query: notablePersonEventsQuery }]}
+                  key={id}
+                >
+                  {changeReviewStatus => (
+                    <Card>
+                      <CardContent>
+                        <Typography component="blockquote" variant="subheading">
+                          {notablePerson.name}
+                        </Typography>
+                        <Typography component="blockquote" variant="headline">
+                          {quote}
+                        </Typography>
+                        <div>Submitted by: {submittedBy.name}</div>
+                        <div>Source: {prettifyUrl(sourceUrl)}</div>
+                        <div>
+                          {date !== undefined ? (
+                            <time dateTime={date.toISOString()}>
+                              {formatDate(date, 'MMMM D, YYYY')}
+                            </time>
+                          ) : null}
+                        </div>
+                      </CardContent>
+                      <CardActions>
+                        <Chip
+                          avatar={iconByReviewStatus[reviewStatus]}
+                          label={labelByReviewStatus[reviewStatus]}
+                          variant="outlined"
+                        />
+                        {reviewStatus !== 'ALLOWED' ? (
+                          <Button
+                            onClick={() => {
+                              changeReviewStatus({
+                                variables: {
+                                  input: {
+                                    eventId: id,
+                                    newValue: 'ALLOWED' as NotablePersonEventReviewStatus,
+                                  },
+                                },
+                              });
+                            }}
+                            size="small"
+                            color="primary"
+                          >
+                            Allow
+                          </Button>
+                        ) : null}
+                        {reviewStatus !== 'REMOVED' ? (
+                          <Button
+                            onClick={() => {
+                              changeReviewStatus({
+                                variables: {
+                                  input: {
+                                    eventId: id,
+                                    newValue: 'REMOVED' as NotablePersonEventReviewStatus,
+                                  },
+                                },
+                              });
+                            }}
+                            size="small"
+                            color="primary"
+                          >
+                            Remove
+                          </Button>
+                        ) : null}
+                      </CardActions>
+                    </Card>
+                  )}
+                </Mutation>
               );
             },
           );
