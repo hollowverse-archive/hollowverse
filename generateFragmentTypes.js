07221253b5e28aee108406bd936634415fe96ba3
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-09-15 15:18:53 +0300

    Modernize generateFragmentTypes

--- generateFragmentTypes.js
+++ generateFragmentTypes.js
@@ -1,55 +1,56 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
-/* eslint-disable no-underscore-dangle */
-const fetch = require('node-fetch');
-const fs = require('fs');
+/* eslint-disable no-underscore-dangle,no-console */
+/* tslint:disable no-console */
+const got = require('got');
+const fs = require('fs-extra');
 
 const { API_ENDPOINT = 'https://api.hollowverse.com/graphql' } = process.env;
 
-fetch(API_ENDPOINT, {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({
-    variables: {},
-    operationName: '',
-    query: `
-      {
-        __schema {
-          types {
-            kind
-            name
-            possibleTypes {
-              name
+(async () => {
+  try {
+    const {
+      body: { data },
+    } = await got.post(API_ENDPOINT, {
+      json: true,
+      body: {
+        variables: {},
+        operationName: '',
+        query: `
+          {
+            __schema {
+              types {
+                kind
+                name
+                possibleTypes {
+                  name
+                }
+              }
             }
           }
-        }
-      }
-    `,
-  }),
-})
-  .then(result => result.json())
-  .then(result => {
-    // here we're filtering out any type information unrelated to unions or interfaces
-    const filteredData = result.data.__schema.types.filter(
+        `,
+      },
+    });
+
+    // Here we're filtering out any type information unrelated to unions or interfaces
+    const filteredData = data.__schema.types.filter(
       type => type.possibleTypes !== null,
     );
 
-    const data = {
-      ...result.data,
+    const result = {
+      ...data,
       __schema: {
-        ...result.data.__schema,
+        ...data.__schema,
         types: filteredData,
       },
     };
 
-    fs.writeFile(
+    await fs.writeFile(
       './src/app/api/fragmentTypes.json',
-      JSON.stringify(data, undefined, 2),
-      err => {
-        if (err) {
-          console.error('Error writing fragmentTypes file', err);
-        } else {
-          console.log('Fragment types successfully extracted!');
-        }
-      },
+      JSON.stringify(result, undefined, 2),
     );
-  });
+
+    console.log('Fragment types successfully extracted!');
+  } catch (error) {
+    console.error('Error writing fragmentTypes file', error);
+  }
+})();
